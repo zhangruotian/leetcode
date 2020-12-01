c89b9a1b8687b70ec3c284a87a6c3ecf69528133
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-01 19:16:00 +1100

    Create 66.加一.py

---
+++ 数组/66.加一.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i]==9:
+                digits[i]=0
+            else:
+                digits[i]+=1
+                return digits
+        digits[0]=1
+        digits.append(0)
+        return digits
+        
+# 199-->200 129-->130 都work
+# 例外999
