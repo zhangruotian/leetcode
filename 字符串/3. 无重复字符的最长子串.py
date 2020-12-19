10c121e0c561c9413125c682f74ecc55752e5a08
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-20 00:29:56 +1100

    Create 3. 无重复字符的最长子串.py

---
+++ 字符串/3. 无重复字符的最长子串.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i,res=0,-1
+        lookup={}
+        for j in range(0,len(s)):
+            if s[j] in lookup:
+                if lookup[s[j]]+1>i:
+                    i=lookup[s[j]]+1
+            lookup[s[j]]=j
+            if j-i>res:
+                res=j-i
+        return res+1
+
+# 滑动窗口 T:O(n)
