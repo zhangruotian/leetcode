914502420fecabcd699dabc988c18a0e8ad72ff9
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-19 20:01:44 +1100

    Create 141.环形链表.py

---
+++ 链表/141.环形链表.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        fast=head
+        slow=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            if fast is slow:
+                return True
+        return False
+
+# 快慢指针 S:O(1) 
