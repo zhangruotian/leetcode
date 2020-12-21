bc44deb250b67a437a1d452381785e7df65fa5d2
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-21 19:32:01 +1100

    Create 234. 回文链表.py

---
+++ 链表/234. 回文链表.py
@@ -0,0 +1,67 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow=head
+        fast=head
+        stack=[]
+        while fast and fast.next:
+            stack.append(slow.val)
+            slow=slow.next
+            fast=fast.next.next
+        
+        if fast:
+            slow=slow.next
+        
+        while slow:
+            if slow.val != stack.pop():
+                return False
+            slow=slow.next
+        return True
+# 快慢指针找中点，利用栈的特性做
+# T:O(n) S:O(n)
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head:
+            return True
+        if not head.next:
+            return True
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        if fast:
+            slow=slow.next
+        new_head=self.inPlaceReverse(slow)
+        while new_head:
+            if new_head.val !=head.val:
+                return False
+            new_head=new_head.next
+            head=head.next
+        return True
+
+    def inPlaceReverse(self,head):
+        pre=None
+        cur=head
+        while cur:
+            next=cur.next
+            cur.next=pre
+            pre=cur
+            cur=next
+        return pre
+     
+# 快慢指针找中点，inplace反转后面链表。判断与前半部分是否一样。
+# T:O(n) S:O(1)
+ 
+        
+
+
+
+
+
