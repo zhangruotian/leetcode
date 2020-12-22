7f1a5bc58f54eece85ba703a40535e5e51449230
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-22 22:31:52 +1100

    add

---
+++ 链表/24. 两两交换链表中的节点.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummy_head=ListNode(0)
+        dummy_head.next=head
+        pre=dummy_head
+        cur=head
+        while cur and cur.next:
+            next=cur.next
+            next_next=next.next
+            pre.next=next
+            next.next=cur
+            cur.next=next_next
+            pre=cur
+            cur=next_next
+        return dummy_head.next
