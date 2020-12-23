4e52b201f4e8c61354063ca2d69a98f17bbd4706
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-23 18:01:58 +1100

    Create 144. 二叉树的前序遍历.py

---
+++ 树/144. 二叉树的前序遍历.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res=[]
+        res+=[root.val]
+        res+=self.preorderTraversal(root.left)
+        res+=self.preorderTraversal(root.right)
+        return res
