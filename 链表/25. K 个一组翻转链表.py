233382baf2d6a876018e1b8f528ac82aed41a4fd
Ruotian Zhang <u7076906@anu.edu.au>
2020-12-26 23:49:38 +1100

    Update 25. K 个一组翻转链表.py

--- 链表/25. K 个一组翻转链表.py
+++ 链表/25. K 个一组翻转链表.py
@@ -5,35 +5,36 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        sub_head=head
-        sub_tail=head
+        dummy_node=ListNode(0)
+        tail=dummy_node
+        sub_head,sub_tail=head,0
+        counter=1
         cur=head
-        count=0
-        tail=ListNode(0)
-        tail.next=head
-        res=tail
         while cur:
-            count += 1
-            if count % k == 0:
-                sub_tail = cur
+            if counter%k==0:
+                next=cur.next
+                counter+=1
+                sub_tail=cur
+                new_head,new_tail=self.reverse(sub_head,sub_tail)
+                tail.next=new_head
+                tail=new_tail
+                sub_head=next
+                cur=next
+            else:                  # 注意此处应该是if else的关系
+                counter+=1
                 cur=cur.next
-                new_head, new_tail = self.reverse(sub_head, sub_tail)
-                tail.next = new_head
-                tail = new_tail
-                sub_head = cur
-            else:
-                cur=cur.next
-        tail.next = sub_head
-        return res.next
-                
-    def reverse(self,head,tail):
-        tail.next=None
+
+        tail.next=sub_head
+        return dummy_node.next
+
+    def reverse(self,sub_head,sub_tail):
+        sub_tail.next=None
         pre=None
-        cur=head
-        while cur.next:
+        cur=sub_head
+        while cur:
             next=cur.next
             cur.next=pre
             pre=cur
             cur=next
-        cur.next=pre
-        return cur,head
+        return pre,sub_head
+        
