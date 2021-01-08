99e1dc461cc442ab3fcf1e487f1ced4a02d928cc
Ruotian Zhang <u7076906@anu.edu.au>
2021-01-08 21:54:16 +1100

    update

--- 哈希表/146. LRU 缓存机制※.py
+++ 哈希表/146. LRU 缓存机制※.py
@@ -65,3 +65,54 @@ class LRUCache:
 
 # hashtable + doubly linked list   保证复杂度都为O(1)  {key:node}  node.key  node.value node.next node.prev 用链表维护顺序
 # https://leetcode-cn.com/problems/lru-cache/solution/lruhuan-cun-ji-zhi-by-leetcode-solution/
+
+class LRUCache(collections.OrderedDict):
+
+    def __init__(self, capacity: int):
+        super().__init__
+        self.capacity=capacity
+
+    def get(self, key: int) -> int:
+        if key in self:
+            self.move_to_end(key)
+            return self[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self:
+            self.move_to_end(key)
+            self[key]=value
+        else:
+            if len(self)<self.capacity:
+                self[key]=value
+            else:
+                self.popitem(last=False)
+                self[key]=value
+# 继承OrderedDict() OrderedDict可以记录添加顺序，将某一个pair移到尾部，popitem....
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.hashmap=collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            self.hashmap.move_to_end(key)
+            return self.hashmap[key]
+        else:
+            return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.hashmap.move_to_end(key)
+            self.hashmap[key]=value
+        else:
+            if len(self.hashmap)<self.capacity:
+                self.hashmap[key]=value
+            else:
+                self.hashmap.popitem(last=False)
+                self.hashmap[key]=value
+# 直接在类内维护一个OrderedDict()也行
