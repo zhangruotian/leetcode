abbd5758f97cccaf1ebd84dfdc993488bb321f68
Ruotian Zhang <u7076906@anu.edu.au>
2021-01-19 21:21:47 +1100

    Create 155. 最小栈.py

---
+++ 栈/155. 最小栈.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack=[]
+        self.min_stack=[float(inf)]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x,self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
