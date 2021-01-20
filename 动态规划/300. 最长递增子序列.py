2348f6b4e15f5c3f7a3ed00c4c2d2e7f69a94a4d
Ruotian Zhang <u7076906@anu.edu.au>
2021-01-20 17:13:53 +1100

    Update 300. 最长递增子序列.py

--- 动态规划/300. 最长递增子序列.py
+++ 动态规划/300. 最长递增子序列.py
@@ -13,3 +13,30 @@ class Solution:
         
 # T:O(n^2) S:O(n)
 # youtube.com/watch?v=fV-TF4OvZpk
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res=[]
+        for i in nums:
+            if not res or i>res[-1]:
+                res.append(i)
+            else:
+                self.lower_bound(res,i)
+        return len(res)
+
+    def lower_bound(self,nums,x):
+        l=0
+        r=len(nums)
+        while l<r:
+            m=(l+r-1)//2
+            if nums[m]<x:
+                l=m+1
+            else:
+                r=m
+        nums[l]=x
+# res[]里面放到目前为止的最优序列（可能不属于sub sequence，但是长度结果是正确的）
+# 方法：如果新值大于res[-1]，res接上
+# 如果新值小于或等于res[-1],二分查找 lower_bound,替换res[x]。
+
+# lower_bound: [1,3,5]中找4，则返回5。找3,返回3.
+# upper_bound: [1,3,5]中找4，则返回5。找3,返回5.
