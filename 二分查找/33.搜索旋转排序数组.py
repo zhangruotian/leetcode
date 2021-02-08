f7788f5748861c1a612b6af2328c3079e705e069
Ruotian Zhang <u7076906@anu.edu.au>
2021-02-08 20:12:57 +1100

    Update 33.搜索旋转排序数组.py

--- 二分查找/33.搜索旋转排序数组.py
+++ 二分查找/33.搜索旋转排序数组.py
@@ -32,3 +32,26 @@ class Solution:
         return -1
         
 # 先找到最小值索引，然后就可二分查找
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)
+        while l<r:
+            m=(l+r-1)//2
+            if target==nums[m]:
+                return m
+            else:
+                if nums[m]>nums[len(nums)-1]:
+                    if target>=nums[0] and target<nums[m]:
+                        r=m
+                    else:
+                        l=m+1
+                else:
+                    if target>nums[m] and target<=nums[len(nums)-1]:
+                        l=m+1
+                    else:
+                        r=m
+        return -1
+# 可直接判断，代码更短，速度更快
