1011373ed591f505eccb0fb91d760a1ea6d85148
Ruotian Zhang <u7076906@anu.edu.au>
2021-02-22 22:41:08 +1100

    Create 662. 二叉树最大宽度.py

---
+++ 树/662. 二叉树最大宽度.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        from collections import deque
+        q=deque()
+        q.append((root,0))
+        res=0
+        while q:
+            arr=[]
+            for _ in range(len(q)):
+                node,index=q.popleft()
+                arr.append(index)
+                if node.left:
+                    q.append((node.left,2*index+1))
+                if node.right:
+                    q.append((node.right,2*index+2))
+            res=max(res,arr[-1]-arr[0]+1)
+        return res
+# 层序遍历
+# 利用完全二叉树性质(heap)，记录下每个node的index
+# 每层计算arr[-1]-arr[0]+1，保存max
