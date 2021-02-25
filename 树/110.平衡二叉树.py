16a00ae398900d2526bcbb1dc0de7ec8e164a141
Ruotian Zhang <u7076906@anu.edu.au>
2021-02-25 20:15:18 +1100

    Update 110.平衡二叉树.py

--- 树/110.平衡二叉树.py
+++ 树/110.平衡二叉树.py
@@ -23,3 +23,26 @@ class Solution:
         return balanced
         
 # T：O(n) 计算高度的同时判断是否balanced
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return False if self.getHeight(root)==-1 else True
+
+    def getHeight(self,root):
+        if not root:return 0
+        l=self.getHeight(root.left)
+        if l==-1:return -1
+        r=self.getHeight(root.right)
+        if r==-1:return -1
+        return max(l,r)+1 if abs(l-r)<=1 else -1
+# 如果左子树已经返回-1了就不需要再递归右子树了，直接返回-1就可以。<O(N)
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:return True
+        return  self.isBalanced(root.left) and self.isBalanced(root.right) and abs(self.getHeight(root.left)-self.getHeight(root.right))<=1
+        
+    def getHeight(self,root):
+        if not root:return 0
+        return max(self.getHeight(root.left),self.getHeight(root.right))+1
+# O(n^2) 直接按题意写
