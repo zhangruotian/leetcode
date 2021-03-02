dd97ce36b07a38306fe6ddb859509e44e37b56d9
Ruotian Zhang <u7076906@anu.edu.au>
2021-03-02 20:56:10 +1100

    Create 328. 奇偶链表.py

---
+++ 链表/328. 奇偶链表.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        odd_head=head
+        even_head=head.next
+        odd=odd_head
+        even=even_head
+        cur=even_head.next
+        last_odd=odd
+        last_even=even
+        i=3
+        while cur:
+            if i%2!=0:
+                odd.next=cur
+                odd=odd.next
+                last_odd=cur
+            else:
+                even.next=cur
+                even=even.next
+                last_even=cur
+            cur=cur.next
+            i+=1
+        last_even.next=None
+        last_odd.next=even_head
+        return odd_head
+# 初始化odd_head和even_head往后添加即可
