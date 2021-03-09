b60ffab28595f5862e78055e46fcfbf20bcdffe6
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-03-09 19:26:01 +1100

    Create 718. 最长重复子数组.py

---
+++ 动态规划/718. 最长重复子数组.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findLength(self, A: List[int], B: List[int]) -> int:
+        #dp[i][j] 以A[i] B[j]结尾的字串的公共长度
+        #if A[i]==B[j]: dp[i][j]=dp[i-1][j-1]+1
+        #else:dp[i][j]=0
+        n1,n2=len(A),len(B)
+        dp=[[0]*(n2+1) for _ in range(n1+1)]
+        self.res=0
+        for i in range(1,n1+1):
+            for j in range(1,n2+1):
+                if A[i-1]==B[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                    if dp[i][j]>self.res:
+                        self.res=dp[i][j]
+        return self.res
+
