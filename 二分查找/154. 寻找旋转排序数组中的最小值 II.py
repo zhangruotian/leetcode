1c884b1c573576976ad6de9646a3ee2e15cbcad1
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-03-17 17:30:26 +1100

    update

--- 二分查找/154. 寻找旋转排序数组中的最小值 II.py
+++ 二分查找/154. 寻找旋转排序数组中的最小值 II.py
@@ -8,10 +8,10 @@ class Solution:
             elif nums[m]>nums[r-1]:
                 l=m+1
             else:
-                return nums[l]
-                
+                r-=1
+        return nums[l]
 
-#  l指向第一个，r-1指向最后一个
+        #  l指向第一个，r-1指向最后一个
 # 如果nums[m]>nums[l-1],说明在第一段。往右找，l=m+1
 # 如果nums[m]<nums[l-1],说明在第二段，往左找。但是当前的m可能对应最小值，因此r=m+1(搜索区间保留m)
-# 如果相等，则区间已缩小到最后，直接return nums[l]
+# 如果nums[m]==nums[l-1],不能确定往左找还是往右找。r-=1,缩小范围。
