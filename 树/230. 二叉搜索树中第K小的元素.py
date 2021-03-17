1a1497ede3cac0561887195ff2287b0c0e0508f3
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-03-18 01:41:45 +1100

    Update 230. 二叉搜索树中第K小的元素.py

--- 树/230. 二叉搜索树中第K小的元素.py
+++ 树/230. 二叉搜索树中第K小的元素.py
@@ -5,21 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    count=0
-    res=float('-inf')
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        self.in_order_traversal(root,k)
-        return self.res
-    
-    def in_order_traversal(self,root,k):
-        if not root:
-            return
-        self.in_order_traversal(root.left,k)
-        self.count+=1
-        if self.count==k:
-            self.res=root.val
-        elif self.count>k:
-            return
-        self.in_order_traversal(root.right,k)
 
-# 中序遍历+剪枝
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        self.cnt=0
+        return self.dfs(root,k)
+    
+    def dfs(self,root,k):
+        if not root:return
+        l=self.dfs(root.left,k)
+        if l!=None:return l
+        self.cnt+=1
+        if self.cnt==k:
+            return root.val
+        r=self.dfs(root.right,k)
+        if r!=None:return r
+
+#找到后停止递归，return
+#注意有的test case return的结果是0，因此不能写if l:  而是if l!=None.
