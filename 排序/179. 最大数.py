b5ee24caae993f7ecad7fd8c8071ec58dbe13c5d
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-04-03 22:17:00 +1100

    Update 179. 最大数.py

--- 排序/179. 最大数.py
+++ 排序/179. 最大数.py
@@ -9,3 +9,24 @@ class Solution:
             res+=str(num)
         return str(int(res))
         #https://www.youtube.com/watch?v=qEIGhVtZ-sg
+        #O(n^2)
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        from functools import cmp_to_key
+        str_nums=[str(i) for i in nums]
+
+        def compare(a,b):
+            return 1 if a+b>b+a else -1
+        
+        str_nums.sort(key=cmp_to_key(compare),reverse=1)
+        return str(int("".join(str_nums)))
+
+        #o(nlogn)
+# python sort(key= ,reverse=1)
+#如果不需要改变排序规则，则用lamda即可。
+#比如,list[[1,2],[2,1],[0,3]]用第0th元素排序。list.sort(key=lambda x:x[0])
+
+#如果需要改变排序规则，比如本题。需要functools.com_to_key。定义
+#一个比较方程compare,然后list.sort(key=com_to_key(compare))
+#定义compare()，返回1或-1.
