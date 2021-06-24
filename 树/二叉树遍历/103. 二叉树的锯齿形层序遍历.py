d7da5abb06371965ddbdd0ce584ad213842eebeb
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-06-24 18:27:34 +1000

    Update 103. 二叉树的锯齿形层序遍历.py

--- 树/二叉树遍历/103. 二叉树的锯齿形层序遍历.py
+++ 树/二叉树遍历/103. 二叉树的锯齿形层序遍历.py
@@ -22,4 +22,28 @@ class Solution:
             res[level].insert(0,root.val)
         self.dfs(root.left,res,level+1)
         self.dfs(root.right,res,level+1)
+
+#BFS
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res=[]
+        cur=[root]
+        level=1
+        while cur:
+            next=[]
+            vals=deque()
+            for node in cur:
+                if level%2==0:
+                    vals.appendleft(node.val)
+                else:
+                    vals.append(node.val)
+                if node.left:
+                    next.append(node.left)
+                if node.right:
+                    next.append(node.right)
+            cur=next
+            level+=1
+            res.append(list(vals))
+        return res
         
