056ba4e54ede613204176ed1c6f9fbd4f712e54a
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-06-28 17:38:09 +1000

    Update 151. 翻转字符串里的单词.py

--- 字符串/151. 翻转字符串里的单词.py
+++ 字符串/151. 翻转字符串里的单词.py
@@ -6,16 +6,20 @@ class Solution:
 
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s=s[::-1]
+        res=''
         word=''
-        words=''
-        for i,c in enumerate(s):
-            if c !=' ':
-                if s[i-1] ==' ' and word:
-                    words+=word+' '
-                    word=c
+        reversed_s=''
+        for c in s:
+            reversed_s=c+reversed_s
+        s=reversed_s
+
+        for i in range(len(s)):
+            if s[i]!=' ':
+                if word and s[i-1]==' ':
+                    res+=word+" "
+                    word=s[i]
                 else:
-                    word=c+word
-        words+=word
-        return words
+                    word=s[i]+word
+        res+=word
+        return res
 # 先整个反转字符串，再翻转每一个单词。 时间O(n)，空间O(n)。
