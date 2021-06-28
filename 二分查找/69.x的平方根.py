7ef6cdc878f0b8f4f85cbfd968df5654747a2f17
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-06-28 19:29:33 +1000

    Update 69.x的平方根.py

--- 二分查找/69.x的平方根.py
+++ 二分查找/69.x的平方根.py
@@ -13,3 +13,14 @@ class Solution:
         return res
         
 # 从[0,x]范围内二分查找
+
+# 牛顿法，可满足精度要求
+class Solution:
+    def mySqrt(self, x: int,precision: int) -> int:
+        if x==0: return 0
+        r1,r2,x=float(x),float(x),float(x)
+        while True:
+            r2=0.5*(r1+x/r1)
+            if abs(r2-r1)<1e-31:
+                return round(r2,precision)
+            r1=r2
