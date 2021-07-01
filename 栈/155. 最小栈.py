d28cfe0eb442983cd9288f8db5da233a9bad18cd
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-01 17:55:46 +1000

    Update 155. 最小栈.py

--- 栈/155. 最小栈.py
+++ 栈/155. 最小栈.py
@@ -20,3 +20,45 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stack[-1]
+
+
+#不使用额外空间
+#栈里维护与最小值之间的差值
+class MinStack:
+    # 1     4.       -3
+    # -3    -1.      -3
+    # 0     2        -2
+    # -2    0        -2
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack=[]
+        self.min_val=0
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append(0)
+            self.min_val=val
+        else:
+            diff=val-self.min_val
+            self.stack.append(diff)
+            if diff<0:
+                self.min_val=val
+            
+
+    def pop(self) -> None:
+        diff=self.stack.pop()
+        if diff>0:
+            res=diff+self.min_val
+        else:
+            res=self.min_val
+            self.min_val=self.min_val-diff
+        return res
+
+
+    def top(self) -> int:
+        return self.stack[-1]+self.min_val if self.stack[-1]>0 else self.min_val
+
+    def getMin(self) -> int:
+        return self.min_val
