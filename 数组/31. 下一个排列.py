abb89dcec23f0e186c7a1f8cf65460305695d1b1
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-01 21:14:33 +1000

    Update 31. 下一个排列.py

--- 数组/31. 下一个排列.py
+++ 数组/31. 下一个排列.py
@@ -3,23 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums)==1:
-            return
-        i=len(nums)-2
-        while i>=0 and nums[i]>=nums[i+1]:
-            i-=1
-        if i==-1:
-            self.reverse(nums,0,len(nums)-1)
-        else:
-            j=len(nums)-1
-            while nums[j]<=nums[i]:
-                j-=1
-            nums[i],nums[j]=nums[j],nums[i]
-            self.reverse(nums,i+1,len(nums)-1)
+        # 4 3 2 1
+        # 1 4 5 3 2 
+        index=-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                index=i 
+                break
+        if index==-1:
+            return self.reverse(nums,0,len(nums)-1)
 
-    def reverse(self,nums,start,end):
-        while start<end:
-            nums[start],nums[end]=nums[end],nums[start]
-            start+=1
-            end-=1
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]>nums[index]:
+                nums[i],nums[index]=nums[index],nums[i]
+                break
+        self.reverse(nums,index+1,len(nums)-1)
+
+    def reverse(self,nums,l,r):
+        while r>l:
+            nums[l],nums[r]=nums[r],nums[l]
+            r-=1
+            l+=1
 #https://www.youtube.com/watch?v=IbcQOdtmvpA
