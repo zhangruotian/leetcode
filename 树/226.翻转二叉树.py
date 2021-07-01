a7231b2ec8f9fca8213c391ee99562b2f81cdd58
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-01 22:19:31 +1000

    Update 226.翻转二叉树.py

--- 树/226.翻转二叉树.py
+++ 树/226.翻转二叉树.py
@@ -13,3 +13,22 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+# 层序遍历 循环
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root: return 
+        cur=[root]
+        while cur:
+            next=[]
+            for node in cur:
+                l=node.left
+                r=node.right
+                node.left=r
+                node.right=l
+                if r: 
+                    next.append(r)
+                if l:
+                    next.append(l)
+            cur=next
+        return root
