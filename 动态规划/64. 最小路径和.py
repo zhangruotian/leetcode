ecb515c3167c75a93317bd0383999808f366c9b5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-06 17:50:27 +1000

    Update 64. 最小路径和.py

--- 动态规划/64. 最小路径和.py
+++ 动态规划/64. 最小路径和.py
@@ -15,3 +15,25 @@ class Solution:
             for j in range(1,col):
                 dp[i][j]=min(dp[i][j-1],dp[i-1][j])+grid[i][j];
         return dp[-1][-1]
+
+#扩展： 返回路径
+#dfs
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        self.min=float(inf)
+        res,path=[],[]
+        m,n=len(grid),len(grid[0])
+        self.dfs(grid,res,path,m,n,0,0)
+        return res[-1]
+    
+    def dfs(self,grid,res,path,m,n,i,j):
+        if i==m and j==n-1 and sum(path)<self.min:
+            self.min=sum(path)
+            res.append(path[:])
+            return 
+        if i>=m or j>=n:
+            return
+        path.append(grid[i][j])
+        self.dfs(grid,res,path,m,n,i,j+1)
+        self.dfs(grid,res,path,m,n,i+1,j)
+        path.pop()
