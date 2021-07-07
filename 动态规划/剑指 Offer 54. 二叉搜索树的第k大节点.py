645ac5b5b40f121c7ff17bf6c51c1e6a3b6ce15c
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-07 17:08:45 +1000

    Update 剑指 Offer 54. 二叉搜索树的第k大节点.py

--- 动态规划/剑指 Offer 54. 二叉搜索树的第k大节点.py
+++ 动态规划/剑指 Offer 54. 二叉搜索树的第k大节点.py
@@ -7,18 +7,47 @@
 
 class Solution:
     def kthLargest(self, root: TreeNode, k: int) -> int:
-        self.k=k
-        return self.dfs(root)
+        self.count=1
+        return self.dfs(root,k)
     
-    def dfs(self,root):
-        if not root:return 
-        r=self.dfs(root.right)
-        if r:return r
-        self.k-=1
-        if self.k==0:
+    def dfs(self,root,k):
+        if not root: return float(inf)
+
+        r=self.dfs(root.right,k)
+        if r!=float(inf):
+            return r
+        if self.count==k:
             return root.val
-        l=self.dfs(root.left)
-        if l:return l 
+        self.count+=1
+
+        l=self.dfs(root.left,k)
+
+        return l
 #找最大k，由于不知道总节点个数，因此不能从小到大找
 #从大到小找，即把中序遍历顺序改反。right->root->left
 #找到之后应该提前return
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        self.k=k 
+        self.res=0
+        self.dfs(root)
+        return self.res
+    
+    def dfs(self,root):
+        if not root:return 
+        self.dfs(root.right)
+        if self.k==0:
+            return 
+        self.k-=1
+        if self.k==0:
+            self.res=root.val
+        self.dfs(root.left)
+#不return的方法，更优美。
