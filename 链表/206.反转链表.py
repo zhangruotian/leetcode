3920e2b61dad3f889cf66ba83984519b904cbaf6
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-11 15:25:13 +1000

    update

--- 链表/206.反转链表.py
+++ 链表/206.反转链表.py
@@ -16,7 +16,6 @@ class Solution:
         return pre
         
         
-        
 # ！！！！！！！！！！！！！！反转链表套路：        
 # 虚拟头结点
 # 在循环外定义pre cur，在循环内求next和next_next(如果有需要)，这样就可以cur and cur.next作为判断while条件
@@ -31,3 +30,21 @@ class Solution:
         head.next=None
         return res
 #递归（分治）
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next: return head
+        dummy_head=ListNode()
+        dummy_head.next=head
+        prev=dummy_head
+        cur=head
+        next=cur.next
+        while next:
+            next_next=next.next
+            cur.next=next_next
+            next.next=prev.next
+            prev.next=next
+            next=next_next
+        return dummy_head.next
+# 头插法
