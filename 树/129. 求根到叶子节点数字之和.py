5efc5387c1fa52b65ae583052760d5b4f9d1020f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-17 17:13:57 +1000

    Update 129. 求根到叶子节点数字之和.py

--- 树/129. 求根到叶子节点数字之和.py
+++ 树/129. 求根到叶子节点数字之和.py
@@ -24,3 +24,24 @@ class Solution:
 # 二叉树的每条从根节点到叶子节点的路径都代表一个数字。
 #其实，每个节点都对应一个数字，等于其父节点对应的数字乘以 1010 再加上该节点的值（这里假设根节点的父节点对应的数字是 00）。
 #只要计算出每个叶子节点对应的数字，然后计算所有叶子节点对应的数字之和，即可得到结果。
+
+
+#bfs循环
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        q=deque([root])
+        res=0
+        while q:
+            for _ in range(len(q)):
+                tmp=q.popleft()
+                l=tmp.left
+                r=tmp.right
+                if not l and not r:
+                    res+=tmp.val
+                if l:
+                    l.val=tmp.val*10+l.val
+                    q.append(l)
+                if r:
+                    r.val=tmp.val*10+r.val
+                    q.append(r)
+        return res
