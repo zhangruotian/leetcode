0c82e40229caa1e340d9085652e9439cef2f26e6
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-07-19 17:33:12 +1000

    Update 112. 路径总和.py

--- 树/112. 路径总和.py
+++ 树/112. 路径总和.py
@@ -15,3 +15,12 @@ class Solution:
             return True
         r=self.hasPathSum(root.right,targetSum-root.val)
         return r 
+
+#用or简化代码
+        
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right and root.val==targetSum:
+            return True
+        return self.hasPathSum(root.left,targetSum-root.val) or self.hasPathSum(root.right,targetSum-root.val)
