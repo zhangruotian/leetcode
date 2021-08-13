e805c3e22b7a92331f012c0d20d3b3a173514aae
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2021-08-13 17:53:42 +1000

    Update 114. 二叉树展开为链表.py

--- 树/114. 二叉树展开为链表.py
+++ 树/114. 二叉树展开为链表.py
@@ -9,29 +9,19 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
+        self.prev=None
         self.dfs(root)
-         
+    
     def dfs(self,root):
-        if not root:return None,None
-        if not root.left and not root.right:return root,root
-        root_l,leaf_l=self.dfs(root.left)
-        root_r,leaf_r=self.dfs(root.right)
-        if not root_l and not leaf_l:
-            root.right=root_r
-            return root,leaf_r
-        if not root_r and not leaf_r:
-            root.right=root_l
-            root.left=None
-            return root,leaf_l
-        leaf_l.right=root_r
-        root.right=root_l
-        root.left=None
-        return root,leaf_r
+        if not root: return 
+        l=root.left
+        r=root.right
+        if self.prev:
+            self.prev.left=None
+            self.prev.right=root
+        self.prev=root
+        self.dfs(l)
+        self.dfs(r)
 
-#分治
-#  root_left,leaf_left=flatten(left)
-#  root_right,leaf_right=flatten(right)
-#  leaf_left.right=root_right
-#  root.right=root_right
-#  注意把root.left置为0
-#  注意处理左侧或者右侧为none的情况
+# 用prev记录前面的节点，prev.left=None prev.right=root
+# 改变了prev的右节点，导致dfs无法正确遍历，因此用l=root.left  r=root.right先记录下来
