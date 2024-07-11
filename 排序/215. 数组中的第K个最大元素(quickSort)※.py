a3d2bcf58271f96d97c14faaf74c753172bb2a52
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-11 14:44:04 +1000

    Update 215. 数组中的第K个最大元素(quickSort)※.py

--- 排序/215. 数组中的第K个最大元素(quickSort)※.py
+++ 排序/215. 数组中的第K个最大元素(quickSort)※.py
@@ -1,131 +1,59 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
         self.quickSort(nums,0,len(nums)-1)
         return nums[-k]
-
-    def partition(self,nums,start,end):
-        pi=start
-        s=start+1
-        e=end
-        while True:
-            while s<e and nums[s]<nums[pi]:   #while s<=end and nums[s]<=pivot:
-                s+=1
-            while s<=e and nums[e]>=nums[pi]:  #while e>0 and nums[e]>pivot:
-                e-=1
-            if s<e:
-                nums[s],nums[e]=nums[e],nums[s]
-            else:
-                break
-        nums[e],nums[pi]=nums[pi],nums[e]
-        return e
-
-    def quickSort(self,nums,start,end):
-        if start>=end:
-            return                            
-        mid=self.partition(nums, start, end)
-        self.quickSort(nums, 0, mid-1)
-        self.quickSort(nums, mid+1, end)
-# 快排 nlogn
-# 递归深度logn，每层把pivot放中间 n。时间复杂度 O(nlogn)。最差情况 ：已排序。时间复杂度O(n2)
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        res_index=len(nums)-k
-        return self.quickSort(nums,0,len(nums)-1,res_index)
-
-    def partition(self,nums,start,end):
-        pi=start
-        s=start+1
-        e=end
-        while True:
-            while s<e and nums[s]<nums[pi]:
-                s+=1
-            while s<=e and nums[e]>=nums[pi]:
-                e-=1
-            if s<e:
-                nums[s],nums[e]=nums[e],nums[s]
-            else:
-                break
-        nums[e],nums[pi]=nums[pi],nums[e]
-        return e
-
-    def quickSort(self,nums,start,end,res_index):              
-        mid=self.partition(nums, start, end)
-        if mid == res_index:
-            return nums[mid]
-        if mid > res_index:
-            return self.quickSort(nums, 0, mid-1,res_index)
-        if mid < res_index:
-            return self.quickSort(nums, mid+1, end,res_index)
-#每次经过「划分」操作后，我们一定可以确定一个元素的最终位置，即 x 的最终位置为 q，并且保证 a[l⋯q−1] 中的每个元素小于等于 a[q]，且 a[q] 小于等于a[q+1⋯r] 中的每个元素。
-#所以只要某次划分的 q 为倒数第 k 个下标的时候，我们就已经找到了答案。 
-#我们只关心这一点，a[l⋯q−1] 和a[q+1⋯r] 是否是有序的，我们不关心。
-
-class Solution:
-    from random import randint
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        res_index=len(nums)-k
-        return self.quickSort(nums,0,len(nums)-1,res_index)
-
-    def partition(self,nums,start,end):
-        pi=start
-        s=start+1
-        e=end
-        while True:
-            while s<e and nums[s]<nums[pi]:
-                s+=1
-            while s<=e and nums[e]>=nums[pi]:
-                e-=1
-            if s<e:
-                nums[s],nums[e]=nums[e],nums[s]
-            else:
-                break
-        nums[e],nums[pi]=nums[pi],nums[e]
-        return e
     
-    def randomPartition(self,nums,start,end):
-        r=randint(start,end)
-        #每次第一个元素与随机元素交换位置，然后再partition，相当于选任意元素作为pivot，可以使分割更均匀。
-        nums[r],nums[start]=nums[start],nums[r]
-        return self.partition(nums,start,end)
+    def partition(self, nums, l ,r):
+        pivot = nums[l]
+        i,j = l+1, r
+        while True:
+            while i<=r and nums[i]<=pivot:
+                i+=1
+            while j>0 and nums[j]>pivot:
+                j-=1
+            if i<j:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                break
+        nums[l], nums[j] = nums[j], nums[l]
+        return j
+    
+    def quickSort(self, nums, l,r):
+        if l>=r:
+            return 
+        mid = self.partition(nums,l,r)
+        self.quickSort(nums, l,mid-1)
+        self.quickSort(nums,mid+1,r)
+# quicksort (best nlogn, worst n^2)
 
-    def quickSort(self,nums,start,end,res_index):              
-        mid=self.randomPartition(nums, start, end)
-        if mid == res_index:
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        length = len(nums)
+        l,r,target = 0,length-1, length-k
+        return self.findKthLargestRecur(nums,l,r,target)
+    
+    def partition(self, nums, l, r):
+        pivot = nums[l]
+        i, j = l+1, r
+        while True:
+            while i<=r and nums[i]<=pivot:
+                i+=1
+            while j>0 and nums[j]>pivot:
+                j-=1
+            if i<j:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                break
+        nums[j], nums[l] = nums[l], nums[j]
+        return j
+    
+    def findKthLargestRecur(self, nums, l, r, target):
+        mid = self.partition(nums,l,r)
+        if mid == target:
             return nums[mid]
-        if mid > res_index:
-            return self.quickSort(nums, 0, mid-1,res_index)
-        if mid < res_index:
-            return self.quickSort(nums, mid+1, end,res_index)
-# 我们知道快速排序的性能和「划分」出的子数组的长度密切相关。
-# 直观地理解如果每次规模为 n 的问题我们都划分成 1和 n - 1，每次递归的时候又向 n - 1 的集合中递归，这种情况是最坏的，
-# 时间代价是 O(n ^ 2)。我们引入随机化加速这一过程。
-# 运行时间由1600ms变为200ms。
-
-
-
-#另外两种partition写法
-
-#以end为pivot，最简单代码
-def partition(self,nums,start,end):
-    pivot=nums[end]
-    pIndex=start
-    for i in range(start,end):
-        if nums[i]<pivot:
-            nums[i],nums[pIndex]=nums[pIndex],nums[i]
-            pIndex+=1
-    nums[end],nums[pIndex]=nums[pIndex],nums[end]
-    return pIndex
-
-#以start为pivot，用在链表的快排
-def partition(self,nums,start,end):
-        pivot=nums[start]
-        pIndex=start+1
-        for i in range(start+1,end+1):
-            if nums[i]<pivot:
-                nums[i],nums[pIndex]=nums[pIndex],nums[i]
-                pIndex+=1
-        nums[start],nums[pIndex-1]=nums[pIndex-1],nums[start]
-        return pIndex-1
-
-
+        if mid < target:
+            return self.findKthLargestRecur(nums,mid+1,r,target)
+        if mid > target:
+            return self.findKthLargestRecur(nums,l,mid-1,target)
+# partition找到那个k的位置就行，无需全部sort
+# leetcode加了case，已经过不去了
