6311fafb79ea02593d987ee8c29ca04a4b60906f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-13 13:17:32 +1000

    Update 25. K 个一组翻转链表※.py

--- 链表/25. K 个一组翻转链表※.py
+++ 链表/25. K 个一组翻转链表※.py
@@ -4,31 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def need_reverse(self,new_head,k):
-        remain = 0
-        cur = new_head
-        while cur:
-            remain+=1
-            cur=cur.next
-        return True if remain>=k else False
-
-    def reverseKGroup(self, head, k):
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy_node = ListNode()
         last_tail = dummy_node
         new_head = head
         while self.need_reverse(new_head,k):
-            last_tail, new_head = self.reverse(last_tail, new_head,k)
-        last_tail.next=new_head
-            
+            last_tail, new_head = self.reverse(last_tail,new_head,k)
+        last_tail.next = new_head
         return dummy_node.next
+
     
-    def reverse(self,last_tail, this_head, k):
-        prev = this_head
-        cur = this_head.next
+    def need_reverse(self,new_head,k):
+        num_left = 0
+        cur = new_head
+        while cur:
+            num_left+=1
+            cur = cur.next
+        return True if num_left>=k else False
+
+
+    def reverse(self, last_tail, new_head,k):
+        prev = new_head
+        cur = new_head.next
         for _ in range(k-1):
             next = cur.next
-            cur.next=prev
-            prev=cur
-            cur=next
+            cur.next = prev
+            prev = cur
+            cur = next
         last_tail.next = prev
-        return this_head, cur
+        return new_head,cur
+        
