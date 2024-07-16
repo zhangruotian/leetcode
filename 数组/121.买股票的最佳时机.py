68979285ef231da288a42dbfc0bc8b7f349a3a98
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-16 18:17:33 +1000

    Update 121.买股票的最佳时机.py

--- 数组/121.买股票的最佳时机.py
+++ 数组/121.买股票的最佳时机.py
@@ -1,19 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices)<2:
-            return 0
-        else:
-            a=0
-            lowest=prices[0]
-            for i in range(1,len(prices)):
-                a = max(a,prices[i]-lowest)
-                if prices[i]<lowest:
-                    lowest=prices[i]
-            return a
-        
-# ‘最大利润’这种字眼大概率用动态规划做
-# 首先找到转移方程 ：f(i)=max(选:f(i-1),不选:prices[i]-lowest)
-# f(i)代表到第i天为止的最优解(最大利润)
-# 与其他动态规划不同之处: 此题需要维护一个到ith天为止的最低价格lowest
-# f(i)只与f(i-1)相关,并且最后结果只用f(n)而不需要前面的f(i)。因此无需维护装f(0),f(1)....f(n)的list。维护一个variable交替计算即可，空间复杂度O(1)
-# 时间复杂度O(n)
+        lowest = prices[0]
+        max_profit = 0
+        for i in range(1, len(prices)):
+            max_profit = max(prices[i]-lowest, max_profit)
+            lowest = min(lowest,prices[i])
+        return max_profit
+# 买的那天一定是卖的那天之前的最小值。 每到一天，维护那天之前的最小值即可。
