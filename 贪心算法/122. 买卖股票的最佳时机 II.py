8ac150ded172635877033ae6410e42293dad08ca
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-16 18:49:25 +1000

    Update 122. 买卖股票的最佳时机 II.py

--- 贪心算法/122. 买卖股票的最佳时机 II.py
+++ 贪心算法/122. 买卖股票的最佳时机 II.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res=0
+        max_profit = 0
         for i in range(1,len(prices)):
-            if prices[i]>prices[i-1]:
-                res+=prices[i]-prices[i-1]
-        return res
-#https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/best-time-to-buy-and-sell-stock-ii-zhuan-hua-fa-ji/
+            p = prices[i]-prices[i-1]
+            if p>=0:
+                max_profit+=p 
+        return max_profit
+# [7, 1, 5, 6] 第二天买入，第四天卖出，收益最大（6-1），所以一般人可能会想，怎么判断不是第三天就卖出了呢? 
+# 这里就把问题复杂化了，根据题目的意思，当天卖出以后，当天还可以买入，所以其实可以第三天卖出，第三天买入，第四天又卖出（（5-1）+ （6-5） === 6 - 1）。
+# 所以算法可以直接简化为只要今天比昨天大，就卖出。
