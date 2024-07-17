bd395ba6565cb2aba41d8f205abb5fee7f5aad3c
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-17 13:12:36 +1000

    Update 236. 二叉树的最近公共祖先.py

--- 树/236. 二叉树的最近公共祖先.py
+++ 树/236. 二叉树的最近公共祖先.py
@@ -7,20 +7,20 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
+        if not root: 
             return
-        if p is root or q is root:
+        if root is p or root is q:
             return root
-
-        left_ancestor=self.lowestCommonAncestor(root.left,p,q)
-        right_ancestor=self.lowestCommonAncestor(root.right,p,q)
-
-        if left_ancestor and right_ancestor:
+        
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left and right:
             return root
-        if left_ancestor:
-            return left_ancestor
-        if right_ancestor:
-            return right_ancestor
+        if left:
+            return left
+        if right:
+            return right
+        
 # 时间复杂度：O(N)O(N)，其中 NN 是二叉树的节点数。二叉树的所有节点有且只会被访问一次，因此时间复杂度为 O(N)O(N)。
 
 # 空间复杂度：O(N)O(N) ，其中 NN 是二叉树的节点数。递归调用的栈深度取决于二叉树的高度，二叉树最坏情况下为一条链，此时高度为 NN，因此空间复杂度为 O(N)O(N)。
