b5e54a65900158bc5b4565ab01a1e35375461a74
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-19 13:58:11 +1000

    Update binarySearch基本实现.py

--- 二分查找/binarySearch基本实现.py
+++ 二分查找/binarySearch基本实现.py
@@ -31,3 +31,21 @@ def binarySearchRec(nums,target):
 nums=[12,15,17,19,21,24,45]
 target=45
 print(binarySearchRec(nums,target))
+
+
+# 请实现有重复数字的升序数组的二分查找
+# 给定一个 元素有序的（升序）长度为n的整型数组 nums 和一个目标值 target，写一个函数搜索 nums 中的第一个出现的target，如果目标值存在返回下标，否则返回 -1
+class Solution:
+    def search(self , nums: List[int], target: int) -> int:
+        # write code here
+        if not nums: return -1
+        l,r = 0, len(nums)
+        while l<r:
+            m = (l+r-1)//2
+            if nums[m]>= target:
+                r=m
+            else:
+                l = m+1
+        return l if nums[l]==target else -1
+# 小于或者等于的时候往左搜，最后ij落在的位置就是第一个target（nums中有target的情况下）。如果没有ij会落在稍大的位置，这时候代码也会返回index，但是应该返回-1，所以加一个
+# 判断 return l if nums[l]==target else -1
