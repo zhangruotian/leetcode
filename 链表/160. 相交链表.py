52b37a5e7592e943566f28f1004631a0b447e47e
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-19 11:55:02 +1000

    Update 160. 相交链表.py

--- 链表/160. 相交链表.py
+++ 链表/160. 相交链表.py
@@ -5,24 +5,21 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        if not headA or not headB:
-            return None
-        curA=headA
-        curB=headB
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        curA, curB = headA, headB
         while True:
+            if not curA and not curB:
+                return None
             if curA is curB:
                 return curA
-            elif not curA.next and not curB.next:
-                return None
-            elif not curA.next:
-                curA=headB
-                curB=curB.next
-            elif not curB.next:
-                curB=headA
-                curA=curA.next
-            else:
-                curA=curA.next
-                curB=curB.next
-# 两个指针，走完A走B，最后会碰到一起
+            if curA and curB:
+                curA = curA.next
+                curB = curB.next
+            if curB and not curA:
+                curA = headB
+                curB = curB.next
+            if curA and not curB:
+                curB = headA
+                curA = curA.next
+# 两个指针，curA走完A走B，curB走完B走A，最后会碰到一起；或者都为none，说明无交点
 # T:O(n+m) S:O(1)
