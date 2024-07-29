a9c7ad0ae652627a8b7b7aa6f013dababc72c0b7
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-29 18:47:10 +1000

    Update 4. 寻找两个正序数组的中位数.py

--- 二分查找/4. 寻找两个正序数组的中位数.py
+++ 二分查找/4. 寻找两个正序数组的中位数.py
@@ -25,3 +25,35 @@ class Solution:
         return (median_l+median_r)/2
 
 # T:O(log(min(m,n)))
+
+# new code
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1, n2 = len(nums1), len(nums2)
+        if not nums1 and not nums2: return None
+        if not nums1: 
+            return nums2[n2//2] if n2%2==1 else (nums2[n2//2-1]+nums2[n2//2])/2
+        if not nums2:
+            return nums1[n1//2] if n1%2==1 else (nums1[n1//2-1]+nums1[n1//2])/2
+        if n1>n2:
+            return self.findMedianSortedArrays(nums2,nums1)
+        l,r = -1, n1
+        while l<r:
+            l1_index = (l+r-1)//2
+            r1_index = l1_index+1
+            l2_index = (n1+n2-1)//2-l1_index-1
+            r2_index = l2_index+1
+            if r1_index == n1 or nums2[l2_index]>nums1[r1_index]:
+                l = l1_index+1
+            elif l1_index == -1 or nums1[l1_index]>nums2[r2_index]:
+                r = l1_index 
+            else:
+                break
+        l1 = nums1[l1_index] if -1<l1_index<n1 else float('-inf')
+        r1 = nums1[r1_index] if -1<r1_index<n1 else float('inf')
+        l2 = nums2[l2_index] if -1<l2_index<n2 else float('-inf')
+        r2 = nums2[r2_index] if -1<r2_index<n2 else float('inf')
+        if (n1+n2)%2 == 1:
+            return max(l1,l2)
+        else:
+            return (max(l1,l2)+min(r1,r2))/2
