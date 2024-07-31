36d6e666c703760a10430ca27b0c863a844db7d2
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-07-31 13:44:23 +1000

    Update 31. 下一个排列.py

--- 数组/31. 下一个排列.py
+++ 数组/31. 下一个排列.py
@@ -25,3 +25,39 @@ class Solution:
             r-=1
             l+=1
 #https://www.youtube.com/watch?v=IbcQOdtmvpA
+
+# new code，找交换位置的时候可以用binary search找upper bound。On + Ologn = On，并未快多少，但是代码不好写，建议上边那种写法。
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        prev = float('-inf')
+        target_index = None
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]>=prev:
+                prev = nums[i]
+            else:
+                target_index = i 
+                break
+        if target_index is None:
+            self.reverse(nums, 0, len(nums)-1) 
+            return 
+        gt_target_index = self.bsearch(nums, target_index+1, len(nums),nums[target_index])
+        nums[target_index], nums[gt_target_index] = nums[gt_target_index], nums[target_index]
+        self.reverse(nums, target_index+1, len(nums)-1)
+
+    def reverse(self, nums, l, r):
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l+=1
+            r-=1
+
+    def bsearch(self, nums, l, r, target):
+        while l<r:
+            m = (l+r-1)//2
+            if nums[m]>target:
+                l = m+1
+            else:
+                r = m
+        return l-1
