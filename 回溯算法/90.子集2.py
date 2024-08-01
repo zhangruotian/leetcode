ae4950be5a030abb3b34169f02c96d0ff9ae8c38
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-01 20:48:45 +1000

    Update 90.子集2.py

--- 回溯算法/90.子集2.py
+++ 回溯算法/90.子集2.py
@@ -1,23 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        path=[]
-        res=[]
-        visited=[0]*len(nums)
         nums.sort()
-        self.backTrack(nums,path,res,visited,0)
+        res,path,visited = [], [], [0]*len(nums)
+        self.dfs(nums,res,path,visited,0)
         return res
     
-    def backTrack(self,nums,path,res,visited,index):
+    def dfs(self,nums,res,path,visited,index):
         res.append(path[:])
-        if index==len(nums):
-            return
+
         for i in range(index,len(nums)):
-            if i>=1 and nums[i]==nums[i-1] and not visited[i-1]:
-                continue
+            if i>0 and nums[i]==nums[i-1] and visited[i-1]==0:
+                continue  
             path.append(nums[i])
             visited[i]=1
-            self.backTrack(nums,path,res,visited,i+1)
+            self.dfs(nums,res,path,visited,i+1)
             path.pop()
             visited[i]=0
-# backtrack
 # 用visited记录是否被访问过。如果nums[i]==nums[i-1]而且visited[i-1]为false，则continue
