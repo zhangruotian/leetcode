cd0479cd430aff4b714b490ba93f5d70c525b3d9
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-02 13:28:39 +1000

    Update 43. 字符串相乘.py

--- 字符串/43. 字符串相乘.py
+++ 字符串/43. 字符串相乘.py
@@ -1,45 +1,44 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1=="0" or num2=="0":
-            return "0"
-        n1,n2=len(num1),len(num2)
-        res_arr=[0]*(n1+n2)
-        for i in range(n1-1,-1,-1):
-            for j in range(n2-1,-1,-1):
-                num1_int,num2_int=int(num1[i]),int(num2[j])
-                res_arr[i+j+1]+=(num1_int*num2_int)%10
-                res_arr[i+j]+=(num1_int*num2_int)//10
-        for i in range(n1+n2-1,-1,-1):
-            if res_arr[i]>=10:
-                ele=res_arr[i]
-                res_arr[i]=ele%10
-                res_arr[i-1]+=ele//10
-        res=""
-        for i in range(n1+n2):
-            if i==0 and res_arr[i]==0:
-                continue
-            res+=str(res_arr[i])
-        return res
-        #https://www.youtube.com/watch?v=G9OWbq-e9hw
+        # 1 2 3         
+        # 4 5 6
 
-class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        #.  9.  9. 9
-        #.  9.  9. 9
-        #8  9   1
-        if num1=='0' or num2=='0':
+        # 9 9 9
+        #.  9 9
+        if num1[0]=='0' or num2[0]=='0':
             return '0'
-        len1,len2=len(num1),len(num2)
-        res=[0]*(len1+len2)
-        for i in range(len2-1,-1,-1):
-            for j in range(len1-1,-1,-1):
-                val=int(num1[j])*int(num2[i])
-                res[i+j+1]+=val%10
-                res[i+j]+=val//10+res[i+j+1]//10
-                res[i+j+1]=res[i+j+1]%10
-        ans=''
-        for i in range(len1+len2):
-            if i==0 and res[i]==0:
-                continue
-            ans+=str(res[i])
-        return ans
+        res = "0"
+        for i in range(len(num2)-1,-1,-1):
+            single_res = self.multi_single(num1,num2[i])+'0'*(len(num2)-1-i)
+            print(single_res)
+            res = self.add(res,single_res)
+        return res
+    
+    def add(self,num1,num2):
+        res = ''
+        carry_over = 0
+        i,j = len(num1)-1,len(num2)-1
+        while i>=0 or j>=0:
+            n1 = ord(num1[i])-ord('0') if i>=0 else 0
+            n2 = ord(num2[j])-ord('0') if j>=0 else 0
+            val = n1+n2+carry_over
+            carry_over = val//10
+            res = str(val%10)+res
+            i-=1
+            j-=1
+        if carry_over:
+            res = '1'+res
+        return res
+    
+    def multi_single(self,num1, n):
+        res = ''
+        carry_over = 0
+        n = ord(n)-ord('0')
+        for i in range(len(num1)-1,-1,-1):
+            n1 = ord(num1[i])-ord('0')
+            val = n*n1+carry_over
+            carry_over = val//10
+            res = str(val%10)+res
+        if carry_over:
+            res = str(carry_over)+res
+        return res
