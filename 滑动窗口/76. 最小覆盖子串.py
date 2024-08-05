ae3aa9b413e4f26925ff5a4e524182385bed7123
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-05 13:54:33 +1000

    Update 76. 最小覆盖子串.py

--- 滑动窗口/76. 最小覆盖子串.py
+++ 滑动窗口/76. 最小覆盖子串.py
@@ -29,3 +29,52 @@ class Solution:
         return min_str 
 
             #https://www.youtube.com/watch?v=YP3bBDuojqk
+
+# new code
+class Solution:
+    from collections import Counter
+    def minWindow(self, s: str, t: str) -> str:
+        # ADOBECODEBANC
+        count = Counter(t)
+        i=0
+        while i<len(s) and s[i] not in count:
+            i+=1
+        if i == len(s):
+            return ""
+        count[s[i]]-=1
+        j = i 
+        while j<len(s) and any([val>0 for val in count.values()]):
+            j=self.expand(s,j,count)
+        if j == len(s):
+            return "" if any([val>0 for val in count.values()]) else s[i:j+1]
+        print(i,j)
+        mini,minj = i,j
+        minlen = j-i
+        while True:
+            i = self.shrink(s,i,j,count)
+            if j-i<minlen:
+                mini,minj = i,j
+                minlen = j-i
+            j = self.expand(s,j,count)
+            if j == len(s):
+                break
+        return s[mini:minj+1]
+    
+    def shrink(self,s,i,j,count):
+        while i<j:
+            if s[i] in count:
+                if count[s[i]]>=0:
+                    break
+                else:
+                    count[s[i]]+=1
+            i+=1
+        return i 
+    
+    def expand(self,s,j,count):
+        while True:
+            j+=1
+            if j==len(s):
+                return j
+            if s[j] in count:
+                count[s[j]]-=1
+                return j
