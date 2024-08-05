0ffc4bfc414a3b2afe8e5e3f9b95c42fb37abe39
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-05 16:10:12 +1000

    Update 32. 最长有效括号.py

--- 栈/32. 最长有效括号.py
+++ 栈/32. 最长有效括号.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        self.max_length=0
-        stack=[-1]
-        for i,c in enumerate(s):
-            if c=='(':
+        # ()(()
+        stack = [-1]
+        res = 0
+        for i in range(len(s)):
+            if s[i] == "(":
                 stack.append(i)
             else:
-                tmp=stack.pop()
-                if not stack:
-                    stack.append(i)
+                stack.pop()
+                if stack:
+                    res = max(res,i-stack[-1])
                 else:
-                    length=i-stack[-1]
-                    self.max_length=max(self.max_length,length)
-        return self.max_length
+                    stack.append(i)
+        return res
 #https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
 # 初始化栈为-1
 
