0f8eb00e5b44e3c00ec24a0bb66c0aa383a9475e
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-13 20:34:58 +1000

    Update 128. 最长连续序列.py

--- 滑动窗口/128. 最长连续序列.py
+++ 滑动窗口/128. 最长连续序列.py
@@ -1,37 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:return 0
-        hashtable={}
+        nums = set(nums)
+        res = 0
         for num in nums:
-            if num in hashtable:
-                continue
-            left=num-1 in hashtable
-            right=num+1 in hashtable
-            if not left and not right:
-                hashtable[num]=1
-            if left and right:
-                l=hashtable[num-1]
-                r=hashtable[num+1]
-                hashtable[num-l]=l+r+1
-                hashtable[num+r]=l+r+1
-                hashtable[num]=1
-            if left and not right:
-                l=hashtable[num-1]
-                hashtable[num]=l+1
-                hashtable[num-l]=l+1
-            if right and not left:
-                r=hashtable[num+1]
-                hashtable[num]=r+1
-                hashtable[num+r]=r+1
-        print(hashtable)
-        return max(hashtable.values())
+            if num-1 not in nums:
+                count = 0
+                while num in nums:
+                    count+=1
+                    num+=1
+                res = max(res, count)
+        return res
 # https://www.youtube.com/watch?v=rc2QdQ7U78I
+# loop through set找下界(num-1不在nums中)，然后依此+1确定长度。不是下界的跳过。
 
-# 首先跳过重复元素，否则会导致结果出错
-# hashtable={num : 以该num为边界的连续数组的长度}
-# [1 2 3] hashmap={1:2,2:2} 要加入3 则找a=hashtable[3-1]，a+1即hashtable[3]的value。同时更新hashmap[3-2]
-# 向左侧延续同理
-# 如果既没有num-1也没有num+1.则hashmap[num]=1
-# 如果都有，则为桥梁，hashtable[num-l]=l+r+1 hashtable[num+r]=l+r+1 hashtable[num]=1(这个不能忘，随便取值都行。如果不写，则相当于没跳过重复元素，会出错)
-# T:o(n) S:o(n)
+
 
