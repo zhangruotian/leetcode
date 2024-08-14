48110d05a421931f27131e8a7b982ce282e2f6ee
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-14 10:58:45 +1000

    Update 470. 用 Rand7() 实现 Rand10().py

--- 数学/470. 用 Rand7() 实现 Rand10().py
+++ 数学/470. 用 Rand7() 实现 Rand10().py
@@ -7,22 +7,18 @@ class Solution:
         """
         :rtype: int
         """
-        rand1=(rand7()-1)*7+rand7()
         while True:
-            if rand1<=40:
-                return rand1%10+1
-            rand2=((rand1-41)%10)*7+rand7()
-            if rand2<=60:
-                return rand2%10+1
-            rand3=((rand2-61)%10)*7+rand7()
-            if rand3<=20:
-                return rand3%10+1
+            rand49 = (rand7()-1)*7+rand7()
+            if rand49<=40:
+                return rand49%10+1
+            else:
+                rand63 = (rand49%10-1)*7+rand7()
+                if rand63<=60:
+                    return rand63%10+1
+                else:
+                    rand21 = (rand63%10-1)*7+rand7()
+                    if rand21<=20:
+                        return rand21%10+1
 
-            
 
-        
-
-        
-
-        #https://www.youtube.com/watch?v=Wyauxe92JJA
-        #https://leetcode-cn.com/problems/implement-rand10-using-rand7/solution/xiang-xi-si-lu-ji-you-hua-si-lu-fen-xi-zhu-xing-ji/
+            #https://leetcode-cn.com/problems/implement-rand10-using-rand7/solution/xiang-xi-si-lu-ji-you-hua-si-lu-fen-xi-zhu-xing-ji/
