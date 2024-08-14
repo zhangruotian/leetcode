986d3ecc90e6a79164c7ac4c60c5af39d260ef03
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-14 12:13:13 +1000

    Update 394. 字符串解码.py

--- 栈/394. 字符串解码.py
+++ 栈/394. 字符串解码.py
@@ -1,28 +1,25 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-            # 100[200[a]]
-        stack1,stack2=[],[]
-        multi=''
+        stack = []
         for c in s:
-            if c.isdigit():
-                multi+=c 
-            elif c.isalpha():
-                stack2.append(c)
-            elif c=='[':
-                stack1.append(int(multi))
-                multi=''
-                stack2.append(c)
+            if c != "]":
+                stack.append(c)
             else:
-                sub=''
-                while True:
-                    tmp=stack2.pop()
-                    if tmp=='[':
-                        break
-                    sub=tmp+sub
-                tmp_num=stack1.pop()
-                sub*=tmp_num
-                stack2.append(sub)
-        return ''.join(stack2)
+                self.decode(stack)
+        return ''.join(stack)
+    
+    def decode(self,stack):
+        string = ''
+        c = stack.pop()
+        while c != '[':
+            string = c+string
+            c = stack.pop()
+        
+        num=''
+        while stack and stack[-1].isdigit():
+            c = stack.pop()
+            num = c+num
 
-# 两个栈，一个记录数字，一个记录字母和'['.遇到右括号时候弹出一个数字栈，字母栈弹到左括号为止.（把数字和字母对应起来）
-#注意数字可能是多位数，用字符串multi记录。遇到'['时将其压人字母栈，清空multi
+        decoded = int(num)*string
+        stack.append(decoded)
+# stack， 遇到']' decode,遇到其他加入stack
