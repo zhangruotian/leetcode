cbdb6babb36a5db18ab3944cfaf332ca597937a5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-15 15:05:50 +1000

    Update 662. 二叉树最大宽度.py

--- 树/662. 二叉树最大宽度.py
+++ 树/662. 二叉树最大宽度.py
@@ -5,23 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def widthOfBinaryTree(self, root: TreeNode) -> int:
-        from collections import deque
-        q=deque()
-        q.append((root,0))
-        res=0
-        while q:
-            arr=[]
-            for _ in range(len(q)):
-                node,index=q.popleft()
-                arr.append(index)
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        res = 0
+        nodes = [(root,0)]
+        while nodes:
+            new_nodes = []
+            for node,index in nodes:
                 if node.left:
-                    q.append((node.left,2*index+1))
+                    new_nodes.append((node.left,2*index+1))
                 if node.right:
-                    q.append((node.right,2*index+2))
-            res=max(res,arr[-1]-arr[0]+1)
+                    new_nodes.append((node.right,2*index+2))
+            res = max(res,nodes[-1][1]-nodes[0][1]+1)
+            nodes = new_nodes
         return res
 # 层序遍历
 # 利用完全二叉树性质(heap)，记录下每个node的index
-# 每层计算arr[-1]-arr[0]+1，保存max
 #虽然树不是完全二叉树，但是可以利用完全二叉树的性质计算每一层节点的位置。
