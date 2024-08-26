38e7fb52a9c8a14f8ebda09206178863a990c3a7
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-08-26 19:33:00 +1000

    Update 227. 基本计算器 II.py

--- 栈/227. 基本计算器 II.py
+++ 栈/227. 基本计算器 II.py
@@ -22,6 +22,7 @@ class Solution:
 
 # new code
 class Solution:
+class Solution:
     def calculate(self, s: str) -> int:
         # 312+21*2 - 31/2+1
         stack = []
@@ -46,11 +47,10 @@ class Solution:
         return sum(stack)
 
     def get_num(self,s,i):
-        num = ord(s[i])-ord('0')
-        i+=1
+        num = 0
         while i<len(s) and s[i].isdigit():
             num = num*10+ord(s[i])-ord('0')
             i+=1
-        return i,num 
+        return i,num
 
 
