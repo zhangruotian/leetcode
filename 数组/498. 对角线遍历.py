d44b14b519e9885ac9c89a8fd0e80d15909de23c
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2024-09-05 17:16:27 +1000

    Update 498. 对角线遍历.py

--- 数组/498. 对角线遍历.py
+++ 数组/498. 对角线遍历.py
@@ -1,41 +1,20 @@
 class Solution:
-    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:return []
-        n,m=len(matrix),len(matrix[0])
-        times=n*m
-        i,j=0,0
-        res=[]
-        for t in range(times):
-            if t%2==0:
-                while True:
-                    if i>=0 and i<n and j>=0 and j<m:
-                        res.append(matrix[i][j])
-                        i-=1
-                        j+=1
-                    elif i<0 and j<m:
-                        i+=1
-                        break
-                    elif j>=m:
-                        j-=1
-                        i+=2
-                        break
-            else:
-                while True:
-                    if i>=0 and i<n and j>=0 and j<m:
-                        res.append(matrix[i][j])
-                        i+=1
-                        j-=1
-                    elif i>=n:
-                        i-=1
-                        j+=2
-                        break
-                    elif i<n and j<0:
-                        j+=1
-                        break
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        res = []
+        m,n = len(mat), len(mat[0])
+        for i in range(m):
+            res.extend(self.one_diagonal(mat,i,0,m,n))
+        for j in range(1,n):
+            res.extend(self.one_diagonal(mat,m-1,j,m,n))
         return res
-# https://leetcode-cn.com/problems/diagonal-traverse/solution/xiao-bai-kan-guo-lai-zui-zhi-bai-yi-li-jie-ban-ben/
 
-
-
-
-
+    def one_diagonal(self,mat,i,j,m,n):
+        reverse = False if (i+j)%2 == 0 else True
+        path = []
+        while i>=0 and j<n:
+            path.append(mat[i][j])
+            i-=1
+            j+=1
+        if reverse:
+            path.reverse()
+        return path 
