6c2c76ad5ec5f7eab0fc2671143041787ac8c1ba
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-05-12 16:28:23 -0700

    Update 15.三数之和.py

--- 数组/15.三数之和.py
+++ 数组/15.三数之和.py
@@ -5,15 +5,15 @@ class Solution:
         for i in range(len(nums)-2):
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            num = nums[i]
             l,r = i+1,len(nums)-1
             while l<r:
-                if num+nums[l]+nums[r]<0 or (l>i+1 and nums[l]==nums[l-1]):
+                if nums[i]+nums[l]+nums[r]<0 or (l>i+1 and nums[l]==nums[l-1]):
                     l+=1
-                elif num+nums[l]+nums[r]>0:
+                elif nums[i]+nums[l]+nums[r]>0:
                     r-=1
                 else:
-                    res.append([num,nums[l],nums[r]])
+                    res.append([nums[i],nums[l],nums[r]])
                     l+=1
                     r-=1
         return res
+            
