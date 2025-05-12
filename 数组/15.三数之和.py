d680aaed94d25d40a0bcc32a995e9c468e6dabe7
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-05-12 16:23:42 -0700

    Update 15.三数之和.py

--- 数组/15.三数之和.py
+++ 数组/15.三数之和.py
@@ -1,70 +1,19 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        res=[]
         for i in range(len(nums)-2):
-            if nums[i]>0:
-                break
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            l=i+1
-            r=len(nums)-1
+            num = nums[i]
+            l,r = i+1,len(nums)-1
             while l<r:
-                if nums[i]+nums[l]+nums[r]==0:
-                    res.append([nums[i],nums[l],nums[r]])
+                if num+nums[l]+nums[r]<0 or (l>i+1 and nums[l]==nums[l-1]):
                     l+=1
+                elif num+nums[l]+nums[r]>0:
                     r-=1
-                    while nums[l]==nums[l-1] and l<r:
-                        l+=1
-                    while nums[r]==nums[r+1] and l<r:
-                        r-=1
-                elif nums[i]+nums[l]+nums[r]<0:
-                    l+=1
                 else:
+                    res.append([num,nums[l],nums[r]])
+                    l+=1
                     r-=1
         return res
-# 首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，数字分别为 nums[L] 和 nums[R]，
-# 如果 nums[i]大于 0，则三数之和必然无法等于 0，结束循环
-# 如果 nums[i] == nums[i-1]，则说明该数字重复，会导致结果重复，所以应该跳过 
-# 计算三个数的和 sum 判断是否满足为 00，满足则添加进结果集
-# 1. 当 sum == 0 时，nums[L] == nums[L+1] 则会导致结果重复，应该跳过，L++
-# 2. 当 sum == 0 时，nums[R] == nums[R-1] 则会导致结果重复，应该跳过，R--
-# 当 sum<0时，L++
-# 当 sum>0时，R--
-
-
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        res = []
-        for i in range(len(nums)-2):
-            if nums[i]>0:
-                return res
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            j,k = i+1, len(nums)-1
-            while j<k:
-                is_fulfill = self.fulfill(nums, res, i,j,k)
-                if is_fulfill==0:
-                    j+=1
-                    k-=1
-                if is_fulfill<0:
-                    j+=1
-                if is_fulfill>0:
-                    k-=1
-        return res
-
-    def fulfill(self,nums,res,i,j,k):
-        sum_ = nums[i]+nums[j]+nums[k]
-        if sum_ ==0:
-            if j==i+1 or k==len(nums)-1 or (nums[j]!=nums[j-1] and nums[k]!=nums[k+1]):
-                res.append([nums[i],nums[j],nums[k]])
-            return 0
-        if sum_ < 0:
-            return -1
-        if sum_>0:
-            return 1
-# new version
-
-
-
