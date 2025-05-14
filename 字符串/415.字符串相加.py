fabb6fb124a2f02e26cbc226556c7f3663179adb
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-05-14 12:32:42 -0700

    Update 415.字符串相加.py

--- 字符串/415.字符串相加.py
+++ 字符串/415.字符串相加.py
@@ -1,20 +1,15 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        # 99      12
-        #999     345
-        i,j,res,add_one=len(num1)-1,len(num2)-1,'',0
-        while i>-1 or j>-1:
-            n1=ord(num1[i])-ord('0') if i>-1 else 0
-            n2=ord(num2[j])-ord('0') if j>-1 else 0
-            add=n1+n2+add_one
-            if add>9:
-                add_one=1
-                res=str(add-10)+res
-            else:
-                add_one=0
-                res=str(add)+res
+        i,j = len(num1)-1,len(num2)-1
+        carry = 0
+        res = ''
+        while i>=0 or j>=0:
+            numi = ord(num1[i])-ord('0') if i>=0 else 0
+            numj = ord(num2[j])-ord('0') if j>=0 else 0
+            sum_ij = numi+numj+carry
+            res_ij = str(sum_ij%10)
+            carry = sum_ij//10
+            res = res_ij+res
             i-=1
             j-=1
-        if add_one:
-            res='1'+res
-        return res
+        return res if carry==0 else '1'+res
