d3025e1ff538a396f2f7c60b7c01a78725fd1189
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-05-23 11:49:10 -0700

    Update 165. 比较版本号.py

--- 字符串/165. 比较版本号.py
+++ 字符串/165. 比较版本号.py
@@ -1,40 +1,18 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        l1=version1.split(".")
-        l2=version2.split(".")
-        n1,n2=len(l1),len(l2)
-        for i in range(max(n1,n2)):
-            bn1=int(l1[i]) if i<n1 else 0
-            bn2=int(l2[i]) if i<n2 else 0
-            if bn1==bn2:
-                continue
-            elif bn1>bn2:
-                return 1
-            else:
-                return -1
-        return 0
-# t:o(m+n+max(m,n)) s:o(m+n)
-
-#不使用自带函数split,自己实现双指针 t:o(max(m,n)) , s:o(1)
-class Solution:
-    def compareVersion(self, version1: str, version2: str) -> int:
-        m,n = len(version1),len(version2)
-        i,j = 0,0
-        while True:
-            x = 0
-            while i<m and version1[i]!='.':
-                x = x*10+ord(version1[i])-ord('0'） #关键是这里的算法
+        i,j=0,0
+        while i<len(version1) or j<len(version2):
+            revision1, revision2 = 0,0
+            while i<len(version1) and version1[i] != '.':
+                revision1 = revision1*10 + ord(version1[i]) - ord('0')
                 i+=1
-            y=0
-            while j<n and version2[j]!='.':
-                y = y*10+ord(version2[j])-ord('0')
+            while j<len(version2) and version2[j] != '.':
+                revision2 = revision2*10 + ord(version2[j]) - ord('0')
                 j+=1
-            if x<y:
+            if revision1<revision2:
                 return -1
-            if x>y:
+            if revision1>revision2:
                 return 1
             i+=1
             j+=1
-            if i>=m and j>=n:
-                return 0
-        
+        return 0
