d2bd30b1bf84cd77dd37396d0ee28739d9866abc
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-05-23 11:02:13 -0700

    Update 22. 括号生成.py

--- 字符串/22. 括号生成.py
+++ 字符串/22. 括号生成.py
@@ -24,18 +24,19 @@ class Solution:
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res=[]
-        self.helper(n-1,n,'(',res)
+        res = []
+        self.helper(res,'',n,n)
         return res
     
-    def helper(self,l,r,item,res):
-        if r==0:
+    def helper(self,res,item,l,r):
+        if l<0 or r<0:
+            return
+        if r<l:
+            return 
+        if l==0 and r==0:
             res.append(item)
-        if l>0:
-            self.helper(l-1,r,item+'(',res)
-        if l<r:
-            self.helper(l,r-1,item+')',res)
-
-# 回溯算法(深度优先遍历)。判断可加左括号条件：l有剩余
-  #     判断可加右括号条件：r有剩余且剩余的l<r。只要有剩余就能加'('，但是')'不能比'('先出现
+            return
+        self.helper(res,item+'(',l-1,r)
+        self.helper(res,item+')',l,r-1)
+  # 左右括号不能用超，右括号不能比左括号先出现
   
