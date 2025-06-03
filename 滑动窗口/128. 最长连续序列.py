85aa1f850675e2148a386cda695d4b1012a5e563
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-02 20:18:38 -0700

    Update 128. 最长连续序列.py

--- 滑动窗口/128. 最长连续序列.py
+++ 滑动窗口/128. 最长连续序列.py
@@ -14,4 +14,34 @@ class Solution:
 # loop through set找下界(num-1不在nums中)，然后依此+1确定长度。不是下界的跳过。
 
 
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # 2 3 4 5
+        #  3 4 5 2
+        #  1 2 4 5 3
+        if not nums:
+            return 0
+        d = {}
+        nums=set(nums)
+        for num in nums:
+            if num-1 not in d and num+1 not in d:
+                d[num]=1
+            if num-1 in d and num+1 in d:
+                length = d[num-1]+d[num+1] +1
+                d[num-d[num-1]] = length
+                d[num+d[num+1]] = length
+                continue
+            if num-1 in d:
+                length = d[num-1]+1
+                d[num] = length
+                d[num-d[num-1]] = length
+            if num+1 in d:
+                length = d[num+1]+1
+                d[num] = length
+                d[num+d[num+1]] = length
+        return max(d.values())
 
+
+        
+        
+        
