dc37e84b810c6065e5c8c1cc6865c67cfc841ab8
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-03 15:18:31 -0700

    Update 152.乘积最大子数组.py

--- 动态规划/152.乘积最大子数组.py
+++ 动态规划/152.乘积最大子数组.py
@@ -9,10 +9,3 @@ class Solution:
             res = max(res,new_maxinum)
             minium,maxinum = new_minimum,new_maxinum
         return res
-# 动态规划
-# 基本思路：一个fmax，记录以ith为结尾的子数组的最大值
-#         fmax=max(nums[i],fmax*nums[i],fmin*nums[i])
-#         一个fmin，记录以ith为结尾的子数组的最小值
-#。       fmin=min(nums[i],temp*nums[i],fmin*nums[i])
-#         res记录最大值，返回
-# S:O(1) T:O(n)
