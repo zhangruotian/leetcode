2f6bd227b1bec8a66885519d4c248181d91cdc6b
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-03 14:22:50 -0700

    Update 662. 二叉树最大宽度.py

--- 树/662. 二叉树最大宽度.py
+++ 树/662. 二叉树最大宽度.py
@@ -6,18 +6,16 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
         res = 0
         nodes = [(root,0)]
         while nodes:
+            res = max(res, nodes[-1][1]-nodes[0][1]+1)
             new_nodes = []
-            for node,index in nodes:
+            for node,i in nodes:
                 if node.left:
-                    new_nodes.append((node.left,2*index+1))
+                    new_nodes.append((node.left,2*i+1))
                 if node.right:
-                    new_nodes.append((node.right,2*index+2))
-            res = max(res,nodes[-1][1]-nodes[0][1]+1)
+                    new_nodes.append((node.right,2*i+2))
             nodes = new_nodes
         return res
 # 层序遍历
