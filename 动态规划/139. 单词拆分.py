1643dd43fdea70c499fbf40100fcc01b547c0703
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-09 11:24:07 -0700

    Update 139. 单词拆分.py

--- 动态规划/139. 单词拆分.py
+++ 动态规划/139. 单词拆分.py
@@ -1,12 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordDict = set(wordDict)
-        dp = [False]*(len(s)+1)
-        dp[0]=True
-        for j in range(1,len(dp)):
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[0] = True
+        for j in range(1,n+1):
             for i in range(j):
                 if dp[i] and s[i:j] in wordDict:
                     dp[j] = True
+                    continue
         return dp[-1]
+
 
 # https://www.youtube.com/watch?v=H2EgWq-45CY
