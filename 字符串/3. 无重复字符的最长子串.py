5bc556aebd5999e8c649d689ecdad52f974b5380
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-09 12:07:06 -0700

    Update 3. 无重复字符的最长子串.py

--- 字符串/3. 无重复字符的最长子串.py
+++ 字符串/3. 无重复字符的最长子串.py
@@ -1,15 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i,res=0,-1
-        lookup={}
-        for j in range(0,len(s)):
-            if s[j] in lookup:
-                if lookup[s[j]]+1>i:
-                    i=lookup[s[j]]+1
-            lookup[s[j]]=j
-            if j-i>res:
-                res=j-i
-        return res+1
+        #bcadab
+        d = {}
+        i = 0
+        res = 0
+        for j in range(len(s)):
+            if s[j] in d and d[s[j]]+1>i:
+                i = d[s[j]]+1
+            res = max(res,j-i+1)
+            d[s[j]] = j
+        return res
 
 # 滑动窗口 T:O(n)
 # https://www.youtube.com/watch?v=LupZFfCCbAU
