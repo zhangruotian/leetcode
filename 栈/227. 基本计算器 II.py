39a0c3613ad359ee48e989a872292274bf9893e8
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-09 10:40:05 -0700

    Update 227. 基本计算器 II.py

--- 栈/227. 基本计算器 II.py
+++ 栈/227. 基本计算器 II.py
@@ -1,47 +1,23 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack=[]
-        num=0
-        pre_sign="+"
-        for i,c in enumerate(s):
-            if c==" " and i!=len(s)-1:continue
-            if c.isdigit():
-                num=num*10+(ord(c)-ord("0"))
-            if (not c.isdigit()) or i==len(s)-1:
-                if pre_sign=="+":
-                    stack.append(num)
-                elif pre_sign=="-":
-                    stack.append(-num)
-                elif pre_sign=="*":
-                    stack.append(stack.pop()*num)
-                elif pre_sign=="/":
-                    stack.append(int(stack.pop()/num))
-                num=0
-                pre_sign=c
-        return sum(stack)
-
-# new code
-class Solution:
-class Solution:
-    def calculate(self, s: str) -> int:
-        # 312+21*2 - 31/2+1
+        # " 3 + 56 *56/ 56 -2 "
+        prev = '+'
+        i = 0 
         stack = []
-        i = 0
-        prev = "+"
         while i<len(s):
             if s[i].isdigit():
-                i,num = self.get_num(s,i)
-                if prev=="+":
+                num,new_i = self.get_num(s,i)
+                if prev=='+':
                     stack.append(num)
-                elif prev=="-":
-                    stack.append(-num)
-                elif prev=="*":
+                if prev=='-':
+                    stack.append(-1*num)
+                if prev=='*':
                     stack[-1] = stack[-1]*num
-                elif prev=="/":
+                if prev=='/':
                     stack[-1] = int(stack[-1]/num)
-            if i==len(s):
-                break
-            if s[i] in {"+","-","*","/"}:
+                i = new_i
+                continue
+            if s[i] in {'+','-','*','/'}:
                 prev = s[i]
             i+=1
         return sum(stack)
@@ -51,6 +27,6 @@ class Solution:
         while i<len(s) and s[i].isdigit():
             num = num*10+ord(s[i])-ord('0')
             i+=1
-        return i,num
-
+        return num,i
+                
 
