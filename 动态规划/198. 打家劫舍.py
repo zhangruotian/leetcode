4ead56acdbaab755c2aa812ee6cfa988eccbe5f5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-24 12:47:37 -0700

    Update 198. 打家劫舍.py

--- 动态规划/198. 打家劫舍.py
+++ 动态规划/198. 打家劫舍.py
@@ -1,12 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #dp[i]=max(dp[i-2]+nums[i],dp[i-1])
-        #2 7 11 11 12
-        if not nums:return 0
-        if len(nums)==1:return nums[0]
-        a=nums[0]
-        b=max(nums[0],nums[1])
-        for i in range(2,len(nums)):
-            a,b=b,max(a+nums[i],b)
+        a,b = 0,nums[0]
+        for i in range(1,len(nums)):
+            a,b = b,max(b,a+nums[i])
         return b
+#dp[i]=max(dp[i-2]+nums[i],dp[i-1])
 #dp[i]只与dp[i-1]和dp[i-2]有关，滚动累计计算即可
