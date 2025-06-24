65b4deaa38888f4fa0edf717debe8cb2ba4d7e12
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-06-24 12:56:55 -0700

    Update 213. 打家劫舍 II.py

--- 动态规划/213. 打家劫舍 II.py
+++ 动态规划/213. 打家劫舍 II.py
@@ -1,32 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #dp[i]=max(dp[i-2]+nums[i],dp[i-1])
-        if len(nums)==1:return nums[0]
-
-        a,b=nums[0],nums[0]
-        for i in range(2,len(nums)-1):
-            a,b=b,max(a+nums[i],b)
-        res1=b
-
-        a,b=0,nums[1]
+        if len(nums)==1:
+            return nums[0]
+        if len(nums)==2:
+            return max(nums[0],nums[1])
+        res1,res2=0,0
+        a,b = 0,nums[1]
         for i in range(2,len(nums)):
-            a,b=b,max(a+nums[i],b)
-        res2=b
+            a,b=b,max(b,a+nums[i])
+        res1 = b
+
+        a,b = 0,nums[0]
+        for i in range(1,len(nums)-1):
+            a,b=b,max(b,a+nums[i])
+        res2 = b
         return max(res1,res2)
-#分两种情况：1.选第1个，则最后一个不能选
-# 2.不选第1个，则最后一个可选
-# max(1,2)
 
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        #dp[i]=max(dp[i-2]+nums[i],dp[i-1])
-        if len(nums)==1:return nums[0] 
-        return max(self.helper(nums[1:]),self.helper(nums[:-1]))
+# 由于房屋是环形排列，第一家和最后一家是相邻的，所以它们 不能同时被抢。
 
-    def helper(self,nums):
-        if len(nums)==1:return nums[0]
-        a,b=nums[0],max(nums[0],nums[1])
-        for i in range(2,len(nums)):
-            a,b=b,max(a+nums[i],b)
-        return b
-# max(rob(nums[1:]),rob(nums[:-1]))
+# 因此，最大金额的抢劫方案必然出自以下两种情况之一：
+# 不抢第一家：这样我们就可以在从第二家到最后一家的范围内（即数组 nums[1:]）进行标准的“打家劫舍”。
+# 不抢最后一家：这样我们就可以在从第一家到倒数第二家的范围内（即数组 nums[:-1]）进行标准的“打家劫舍”。
