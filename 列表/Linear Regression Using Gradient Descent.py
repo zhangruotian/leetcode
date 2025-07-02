caac83359d1e39cb57ff11fe7cf5268cafd1b92c
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-02 11:17:20 -0700

    Update Linear Regression Using Gradient Descent.py

--- 列表/Linear Regression Using Gradient Descent.py
+++ 列表/Linear Regression Using Gradient Descent.py
@@ -1,10 +1,19 @@
-import numpy as np
-def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:
+def linear_regression_gradient_descent(
+    X: np.ndarray, y: np.ndarray, alpha: float, iterations: int
+) -> np.ndarray:
     m, n = X.shape
-    theta = np.zeros((n, 1))
-    y = y.reshape(-1,1)
+    # Using X_b to avoid overwriting the original X variable, which is useful for plotting.
+    X_b = np.concatenate((np.ones((m, 1)), X), axis=1)
+    theta = np.zeros((n + 1, 1))
+    losses = []
+    y = y.reshape(-1, 1)
     for _ in range(iterations):
-        L = (X @ theta - y).T @ (X @ theta - y)
-        gradient = (1 / m) * X.T @ (X @ theta - y)
-        theta -= alpha * gradient
-    return theta
+        pred = X_b @ theta
+        diff = pred - y
+        grad = (1 / m) * X_b.T @ diff
+        theta -= alpha * grad
+        loss = (1 / m) * (X_b @ theta - y).T @ (X_b @ theta - y)
+        losses.append(loss.item())
+    return theta.round(4).flatten().tolist(), losses
+
+# embed b in to theta.
