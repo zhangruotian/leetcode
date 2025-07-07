9836ada7ee81b38e08e73525f9d6338d3e0bdbb5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-07 16:32:54 -0700

    Update Train Logistic Regression with Gradient Descent.py

--- 列表/Train Logistic Regression with Gradient Descent.py
+++ 列表/Train Logistic Regression with Gradient Descent.py
@@ -1,47 +1,57 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-def train_logreg(
-    X: np.ndarray, y: np.ndarray, learning_rate: float, iterations: int
-) -> tuple[list[float], ...]:
-    """
-    Gradient-descent training algorithm for logistic regression, optimizing parameters with Binary Cross Entropy loss.
-    """
-    m, n = X.shape
-    X = np.concatenate((np.ones((m, 1)), X), axis=1)
-    y = y.reshape(-1, 1)
-    theta = np.zeros((n+1, 1))
-    losses = []
-    
-    for _ in range(iterations):
-        pred = sigmoid(X @ theta)
-        diff = pred - y
-        grad = (1/m)*X.T @ diff
-        theta -= learning_rate * grad
-        loss = (1/m)*(-np.sum(y * np.log(pred) + (1 - y) * np.log(1 - pred)))
-        losses.append(loss.round(4).tolist())
-    return np.round(theta, 4).flatten().tolist(),losses
+class LogisicRegression:
+    def __init__(self, X, y, lambda_, num_epochs, lr):
+        self.X = X
+        self.y = y.reshape(-1, 1)
+        self.n, self.d = X.shape
+        self.X_b = np.concatenate((np.ones((self.n, 1)), self.X), axis=1)
+        self.theta = np.zeros((self.d + 1, 1))
+        self.lambda_ = lambda_
+        self.num_epochs = num_epochs
+        self.lr = lr
 
-def logistic_regression_gradient_descent_with_regularization(
-    X: np.ndarray, y: np.ndarray, learning_rate: float, iterations: int, lambda_: float
-):
-    m, n = X.shape
-    X = np.concatenate((np.ones((m, 1)), X), axis=1)
-    y = y.reshape(-1, 1)
-    theta = np.zeros((n+1, 1))
-    losses = []
-    
-    for _ in range(iterations):
-        pred = sigmoid(X @ theta)
-        loss = (1/m)*(-np.sum(y * np.log(pred) + (1 - y) * np.log(1 - pred)) + lambda_ * np.sum(theta[1:] ** 2))
-        losses.append(loss.round(4).tolist())
-        
-        diff = pred - y
-        theta_without_bias = np.copy(theta)
-        theta_without_bias[0] = 0
-        grad = (1/m)*(X.T @ diff + lambda_ * theta_without_bias)
-        theta -= learning_rate * grad
+    def train(self):
+        for _ in range(self.num_epochs):
+            pred = self.sigmoid(self.X_b @ self.theta)
+            loss = (1 / self.n) * (
+                -np.sum(self.y * np.log(pred)) - np.sum((1 - self.y) * np.log(1 - pred))
+            ) + (1 / self.n) * self.lambda_ * np.sum(self.theta[1:] ** 2)
+            print(loss)
+            diff = pred - self.y
+            theta_w = np.copy(self.theta)
+            theta_w[0, 0] = 0
+            grad = (1 / self.n) * (self.X_b.T @ diff) + (
+                1 / self.n
+            ) * self.lambda_ * theta_w
+            self.theta -= self.lr * grad
 
-    return np.round(theta, 4).flatten().tolist(),losses
+    def sigmoid(self, v):
+        return 1 / (1 + np.exp(-1 * v))
 
-def sigmoid(v: np.ndarray) -> np.ndarray:
-    return 1 / (1 + np.exp(-v))
+    def predict(self, data):
+        data = np.concatenate((np.ones((data.shape[0], 1)), data), axis=1)
+        pred = self.sigmoid(data @ self.theta)
+        return np.round(pred)
+
+
+if __name__ == "__main__":
+    np.random.seed(42)  # 使用种子确保每次运行结果一致
+    num_points = 100
+    X0 = np.random.randn(num_points,2)-1
+    y0 = np.zeros((num_points,1))
+    X1 = np.random.randn(num_points,2)+1
+    y1 = np.ones((num_points,1))
+    X_train = np.vstack((X0,X1))
+    y_train = np.vstack((y0,y1))
+    model = LogisicRegression(X_train,y_train,0.1,1000,0.01)
+    model.train()
+    plt.scatter(X0[:,0],X0[:,1],color = 'blue')
+    plt.scatter(X1[:,0],X1[:,1],color = 'red')
+    x0_line = np.linspace(-10,10,100)
+    b,theta1,theta2 = model.theta
+    # b+theta1*x0+theta2*x1 = 0
+    x1_line = (-b-theta1*x0_line)/theta2
+    plt.plot(x0_line,x1_line)
+    plt.show()
