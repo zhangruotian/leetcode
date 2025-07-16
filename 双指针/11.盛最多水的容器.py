014e1fc73f538882f74f71277d666d1a28c0376e
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-16 13:22:00 -0700

    Update 11.盛最多水的容器.py

--- 双指针/11.盛最多水的容器.py
+++ 双指针/11.盛最多水的容器.py
@@ -14,3 +14,6 @@ class Solution:
 #双指针法，前后放置指针。短板决定水高，因此移动较短的指针，左侧向右，右侧向右，直到相遇。
 #如果移动较高指针，后面所有的面积都会比当下的面积小。我们要找最大的，因此应移动较矮的指针。
 # 时间复杂度O(n)，空间复杂度O(1)
+
+# 正确性证明
+# https://leetcode.cn/problems/container-with-most-water/solutions/11491/container-with-most-water-shuang-zhi-zhen-fa-yi-do/
