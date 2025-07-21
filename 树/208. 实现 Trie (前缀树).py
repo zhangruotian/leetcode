b4178106aa305b90181162aff7cb361f395954e0
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-21 10:02:41 -0700

    Update 208. 实现 Trie (前缀树).py

--- 树/208. 实现 Trie (前缀树).py
+++ 树/208. 实现 Trie (前缀树).py
@@ -49,3 +49,34 @@ class Trie:
 #理论https://www.youtube.com/watch?v=pkaooVBexeU
 #python实现时，children使用字典
 #https://www.youtube.com/watch?v=KX2GdDZPxQA
+
+# 用hashtable嵌套
+class Trie:
+
+    def __init__(self):
+        self.d={}
+
+    def insert(self, word: str) -> None:
+        level = self.d
+        for c in word:
+            if not c in level:
+                level[c] = {}
+            level = level[c]
+        level['is_word'] = True
+
+    def search(self, word: str) -> bool:
+        level = self.d
+        for c in word:
+            if not c in level:
+                return False
+            level = level[c]
+        return 'is_word' in level 
+
+    def startsWith(self, prefix: str) -> bool:
+        level = self.d
+        for c in prefix:
+            if not c in level:
+                return False
+            level = level[c]
+        return True
+
