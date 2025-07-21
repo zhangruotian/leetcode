5c16e1587d7b37814fe73229a2a24ae86c786857
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-21 14:59:32 -0700

    Update 211. 添加与搜索单词 - 数据结构设计.py

--- BFS/211. 添加与搜索单词 - 数据结构设计.py
+++ BFS/211. 添加与搜索单词 - 数据结构设计.py
@@ -1,36 +1,33 @@
-class Node:
-    def __init__(self):
-        self.is_word=False
-        self.children={}
-
+from collections import deque
 class WordDictionary:
-
     def __init__(self):
-        self.root = Node()
+        self.d = {}
 
     def addWord(self, word: str) -> None:
-        cur = self.root
+        cur = self.d
         for c in word:
-            if not c in cur.children:
-                cur.children[c] = Node()
-            cur = cur.children[c]
-        cur.is_word = True
+            if not c in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['is_word'] = True
 
     def search(self, word: str) -> bool:
-        curs = [self.root]
+        q = deque()
+        q.append(self.d)
         for c in word:
-            new_curs = []
-            for cur in curs:
-                if c == '.':
-                    new_curs+=list(cur.children.values())
-                if c in cur.children:
-                    new_curs.append(cur.children[c])
-            if not new_curs:
+            for _ in range(len(q)):
+                popped = q.popleft()
+                if c in popped:
+                    q.append(popped[c])
+                if c =='.':
+                    for v in popped.values():
+                        if type(v)==dict:
+                            q.append(v)
+            if not q:
                 return False
-            curs=new_curs
-
-        for cur in curs:
-            if cur.is_word:
+        for r in q:
+            if 'is_word' in r:
                 return True
         return False
+        
 # 208题加强版，用所有词创建trie。208题cur为一个node，本题注意搜索的时候cur可能有很多个node，需要把它们用list中，再往下走。
