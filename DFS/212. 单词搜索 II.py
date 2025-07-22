1c0e767716f328e1077682e87403730346a3e7d1
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-22 15:05:42 -0700

    Update 212. 单词搜索 II.py

--- DFS/212. 单词搜索 II.py
+++ DFS/212. 单词搜索 II.py
@@ -1,53 +1,40 @@
-class Node:
-    def __init__(self):
-        self.is_word = False
-        self.word = None
-        self.children = {}
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-
-    def insert(self,word):
-        cur = self.root
-        for c in word:
-            if not c in cur.children:
-                cur.children[c] = Node()
-            cur = cur.children[c]
-        cur.is_word = True
-        cur.word = word
-    
-    def search(self,board,i,j,cur,m,n,res):
-        if i<0 or i>=m or j<0 or j>=n:
-            return 
-        if board[i][j]=='#':
-            return
-        if cur.is_word:
-            res.add(cur.word)
-            cur.is_word=False
-        tmp = board[i][j]
-        board[i][j]='#'
-        if i+1<m and board[i+1][j] in cur.children:
-            self.search(board,i+1,j,cur.children[board[i+1][j]],m,n,res)
-        if i-1>=0 and board[i-1][j] in cur.children:
-            self.search(board,i-1,j,cur.children[board[i-1][j]],m,n,res)
-        if j+1<n and board[i][j+1] in cur.children:
-            self.search(board,i,j+1,cur.children[board[i][j+1]],m,n,res)
-        if j-1>=0 and board[i][j-1] in cur.children:
-            self.search(board,i,j-1,cur.children[board[i][j-1]],m,n,res)
-        board[i][j] = tmp
-
 class Solution:
+    def build_trie(self,d,word):
+        cur = d
+        for c in word:
+            if not c in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['is_word'] = word
+    
+    def dfs(self,board,i,j,cur,res,m,n,visited):
+        if 'is_word' in cur:
+            res.add(cur['is_word'])
+        if i<0 or i>=m or j<0 or j>=n:
+            return
+        if visited[i][j]:
+            return 
+        if board[i][j] not in cur:
+            return
+        visited[i][j] = 1
+        self.dfs(board,i+1,j,cur[board[i][j]],res,m,n,visited)
+        self.dfs(board,i-1,j,cur[board[i][j]],res,m,n,visited)
+        self.dfs(board,i,j+1,cur[board[i][j]],res,m,n,visited)
+        self.dfs(board,i,j-1,cur[board[i][j]],res,m,n,visited)
+        visited[i][j] = 0
+
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        d = {}
         res = set()
-        trie = Trie()
-        for word in words:
-            trie.insert(word)
         m,n = len(board),len(board[0])
+        visited = [[0]*n for _ in range(m)]
+        for word in words:
+            self.build_trie(d,word)
         for i in range(m):
             for j in range(n):
-                if board[i][j] in trie.root.children:
-                    trie.search(board,i,j,trie.root.children[board[i][j]],m,n,res)
+                if board[i][j] in d:
+                    self.dfs(board,i,j,d,res,m,n,visited)
         return list(res)
+
 
 # 用 Trie 把所有单词的前缀挂起来，再从每个格子做 DFS，只往 Trie 里存在的分支走；遇到完整单词就加入结果并标记删除
