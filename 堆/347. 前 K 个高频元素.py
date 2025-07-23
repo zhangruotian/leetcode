7f312c2f00d982e114c774a496f3f758c5ab7ee9
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-23 12:24:15 -0700

    Update 347. 前 K 个高频元素.py

--- 堆/347. 前 K 个高频元素.py
+++ 堆/347. 前 K 个高频元素.py
@@ -1,20 +1,17 @@
+from collections import Counter
+import heapq
 class Solution:
-    import heapq
-    from collections import Counter
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = tuple(Counter(nums).items())
-        min_heap = []
-        i = 0
-        while len(min_heap)<k:
-            heapq.heappush(min_heap,(count[i][1],count[i][0]))
-            i+=1
-        while i<len(count):
-            if count[i][1]>min_heap[0][0]:
-                heapq.heappushpop(min_heap,(count[i][1],count[i][0]))
-            i+=1
         res = []
-        for c,n in min_heap:
-            res.append(n)
+        counter = Counter(nums)
+        t = [(count,num) for num,count in counter.items()]
+        min_heap=[]
+        for i in range(k):
+            heapq.heappush(min_heap,t[i])
+        for i in range(k,len(t)):
+            heapq.heappushpop(min_heap,t[i])
+        for _,num in min_heap:
+            res.append(num)
         return res
   # 维护一个element数目为k的min heap，出现次数最小的在最上面。后面只要来的比最顶的大，就heappushandpop，到最后，heap里面就是k个次数最多的。
 
