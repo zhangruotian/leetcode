d7cd85f0c802749eda9652aadfec4e65881d4025
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-24 12:41:59 -0700

    Update 271.字符串的编码与解码.py

--- 字符串/271.字符串的编码与解码.py
+++ 字符串/271.字符串的编码与解码.py
@@ -1,29 +1,22 @@
 class Solution:
-  
+
     def encode(self, strs: List[str]) -> str:
-        encoded=''
-        for s in strs:
-            encoded += str(len(s))+'#'+s
-        return encoded
-      
+        #[n#e#e#t,c#o#d#e]
+        s = ''
+        for w in strs:
+            s+=str(len(w))+'#'+w
+        return s
+
     def decode(self, s: str) -> List[str]:
-        res = []
-        i=0
+        l = []
+        i = 0
         while i<len(s):
-            new_i=self.getstr(s,i,res)
-            i = new_i
-        return res
-      
-    def getstr(self,s,i,res):
-        num = 0
-        while i<len(s):
-            c = s[i]
-            if c.isdigit():
-                num = num*10+int(c)
-            if c=='#':
-                break
-            i+=1
-        res.append(s[i+1:i+1+num])
-        return i+1+num
+            length = 0
+            while s[i].isdigit():
+                length = length*10+int(s[i])
+                i+=1
+            l.append(s[i+1:i+1+length])
+            i = i+1+length
+        return l
       
 # https://www.youtube.com/watch?v=B1k_sxOSgv8
