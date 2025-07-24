1009d3c706b5da376bb2ea8bd03a3bdc2384130a
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-24 11:34:34 -0700

    Update 323. 无向图中连通分量的数目.py

--- 图/323. 无向图中连通分量的数目.py
+++ 图/323. 无向图中连通分量的数目.py
@@ -1,27 +1,26 @@
 from collections import defaultdict
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        g = defaultdict(list)
-        for a,b in edges:
-            g[a].append(b)
-            g[b].append(a)
-        cur_nodes = set(g.keys())
-        res = n-len(cur_nodes)
-        while cur_nodes:
-            stack = []
-            visited = set()
-            start = next(iter(cur_nodes))
-            stack.append(start)
-            visited.add(start)
-            while stack:
-                popped = stack.pop()
-                cur_nodes.remove(popped)
-                for node in g[popped]:
-                    if node not in visited:
-                        stack.append(node)
-                        visited.add(node)
+        graph = defaultdict(list)
+        for n1,n2 in edges:
+            graph[n1].append(n2)
+            graph[n2].append(n1)
+        visited = [0]*n
+        res = 0
+        for start in range(n):
+            if visited[start]:
+                continue
             res+=1
+            if start in graph:
+                self.dfs(graph,start,visited)
         return res
+    
+    def dfs(self,graph,node,visited):
+        if visited[node]:
+            return
+        visited[node] =1
+        for child in graph[node]:
+            self.dfs(graph,child,visited)
 
 # 从一个node出发，traverse，这次到达的所有节点组成一个land。然后找一个从未到达的node重新出发，traverse。最终所有node会被走完，记录一共出发了几次。
 # 注意孤立的node，没有任何信息在edges list里面，需要在res中把它们加上。
