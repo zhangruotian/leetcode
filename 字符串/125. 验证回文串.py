866271bc395543a282dcc4aaa29fec472ecb5a41
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-07-30 11:24:15 -0700

    Update 125. 验证回文串.py

--- 字符串/125. 验证回文串.py
+++ 字符串/125. 验证回文串.py
@@ -1,14 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s1=""
-        for c in s:
-            if c.isdigit() or c.isalpha():
-                s1+=c
-        s1=s1.lower()
-        l,r=0,len(s1)-1
-        while l<r:
-            if s1[l]!=s1[r]:
+        i,j = 0,len(s)-1
+        while True:
+            while i<len(s) and not s[i].isalnum():
+                i+=1
+            while j>=0 and not s[j].isalnum():
+                j-=1
+            if i>=j or i==len(s) or j==-1:
+                return True
+            a = s[i].lower() if s[i].isalpha() else s[i]
+            b = s[j].lower() if s[j].isalpha() else s[j]
+            if a!=b:
                 return False
-            l+=1
-            r-=1
-        return True
+            i+=1
+            j-=1
+
+# 双指针O(1) space
