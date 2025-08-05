9969838ca63d60c1734ebb9a7c4297f9a761ae6f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-08-05 11:55:00 -0700

    Update 853. 车队.py

--- 栈/853. 车队.py
+++ 栈/853. 车队.py
@@ -7,4 +7,5 @@ class Solution:
             if not stack or times[i]>stack[-1]:
                 stack.append(times[i])
         return len(stack)
+# 单调栈
 # 将车辆按位置（从近到远）排序后，用一个栈（Stack）来模拟：遍历车辆，若当前车到达时间比栈顶慢，则入栈形成新车队；否则合并。最终栈的大小即为车队数量。
