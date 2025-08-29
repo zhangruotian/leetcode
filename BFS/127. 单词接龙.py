ea89a4b6b59429f42419f82daf15269d9c861b4f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-08-29 13:26:15 -0700

    Update 127. 单词接龙.py

--- BFS/127. 单词接龙.py
+++ BFS/127. 单词接龙.py
@@ -30,3 +30,4 @@ class Solution:
         return next_words
 # DFS适用于沿着一条路track完整的路径path，并且用visited保证这条路径不重复添加word。DFS一般要走完整个图/树才能知道结果。本题让找最短的，因此适合BFS,因为在某层找到符合要求的endWord就可以停止了。
 # BFS过程中注意中，不需要知道某个word是上层谁的child。可以去重保障q上无duplicates，而且无上层已访问过的word，因为最终的结果肯定是没有重复word的。
+# https://www.youtube.com/watch?v=hB_nYXFtwP0
