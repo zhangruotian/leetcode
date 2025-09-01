9296fa090ec0ee9d75724e58d26bc1cd0a5e7c00
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-01 14:32:16 -0700

    Update 45. 跳跃游戏 II.py

--- 贪心算法/45. 跳跃游戏 II.py
+++ 贪心算法/45. 跳跃游戏 II.py
@@ -1,30 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums)==1: return 0
-        step=0
-        i=0
-        while nums[i]+i<len(nums)-1:
-            max_val=float(-inf)
-            max_id=0
-            for j in range(nums[i]):
-                if nums[i+j+1]+j+i+1>max_val:
-                    max_id=i+j+1
-                    max_val=nums[i+j+1]+j+i+1
-            i=max_id
-            step+=1
-        return step+1
-#greedy
-#https://leetcode-cn.com/problems/jump-game-ii/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-10/
-
-#dp超时
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        # for j in range(i):
-        #   dp[i]=min(dp[j])+1
-        dp=[float(inf)]*len(nums)
-        dp[0]=0
-        for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[j]+j>=i:
-                    dp[i]=min(dp[j]+1,dp[i])
-        return dp[-1]
+        num_jumps = 0
+        last_reached,reached = -1,0
+        while reached<len(nums)-1:
+            new_reached = reached
+            for i in range(last_reached+1,new_reached+1):
+                new_reached = max(new_reached, nums[i]+i)
+            last_reached = reached
+            reached = new_reached
+            num_jumps+=1
+        return num_jumps
+# greedy，每一跳记录不同位置能到达最远的位置，知道到达len(nums)-1。O(n)
