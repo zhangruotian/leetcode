94346f1f4365590c1e9b5bbb6aa946dcc75d815f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-01 18:27:10 -0700

    Update 134. 加油站.py

--- 贪心算法/134. 加油站.py
+++ 贪心算法/134. 加油站.py
@@ -1,12 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas)-sum(cost)<0:return -1
-        start=0
-        cur=0
+        if sum(gas)<sum(cost):
+            return -1
+        res = 0
+        acc = 0
         for i in range(len(gas)):
-            cur=cur+gas[i]-cost[i]
-            if cur<0:
-                start=i+1
-                cur=0
-        return start
+            acc+=gas[i]-cost[i]
+            if acc<0:
+                res = i+1
+                acc = 0
+        return res
 #笔记
