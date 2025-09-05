14a86dbef21eb8b085675d60a50111f5041fd731
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-05 11:58:12 -0700

    Update 5.最长回文子串.py

--- 动态规划/5.最长回文子串.py
+++ 动态规划/5.最长回文子串.py
@@ -1,20 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        start,end=0,0
+        res_i,res_j = 0,0
         for i in range(len(s)):
-            l1,r1=self.getPalindrome(s,i,i+1)
-            l2,r2=self.getPalindrome(s,i,i)
-            if r1-l1>=r2-l2 and r1-l1>end-start:
-                start,end=l1,r1
-            if r2-l2>r1-l1 and r2-l2>end-start:
-                start,end=l2,r2
-        return s[start:end+1]
-
-    def getPalindrome(self,s,l,r):
-        while l>=0 and r<=len(s)-1 and s[l]==s[r]:
+            res_i,res_j = self.extend(i,i,s,res_i,res_j)
+            res_i,res_j  = self.extend(i,i+1,s,res_i,res_j)
+        return s[res_i:res_j+1]
+    
+    def extend(self,l,r,s,res_i,res_j):
+        while l>=0 and r<len(s) and s[l]==s[r]:
             l-=1
             r+=1
-        return l+1,r-1
+        if r-l-2>res_j-res_i:
+            return l+1,r-1
+        return res_i,res_j
        
 # 从中间往两边扩展，两种情况:1. 中间单独aba 2.中间一对 baab
 # 时间复杂度:O(n^2) 空间:O(1)
