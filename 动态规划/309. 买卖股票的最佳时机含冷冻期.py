72965b956fdbb1c68b26e0832b40a413c6333dc9
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-07 12:20:12 -0700

    Update 309. 买卖股票的最佳时机含冷冻期.py

--- 动态规划/309. 买卖股票的最佳时机含冷冻期.py
+++ 动态规划/309. 买卖股票的最佳时机含冷冻期.py
@@ -3,7 +3,7 @@ class Solution:
         # rest[i] = max(rest[i-1],sold[i-1])
         # hold[i] = max(hold[i-1],rest[i-1]-prices[i])
         # sold[i] = hold[i-1]+price[i]
-        rest,hold,sold = 0,-prices[0],0
+        rest,hold,sold = 0,-prices[0],float('-inf')
         for i in range(1,len(prices)):
             rest,hold,sold = max(rest,sold),max(hold,rest-prices[i]),hold+prices[i]
         return max(rest,hold,sold)
