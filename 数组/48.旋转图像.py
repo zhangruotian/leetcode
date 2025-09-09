91c76567b371729e0cd9f8e23f4e315f7095629c
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-09 15:09:18 -0700

    Update 48.旋转图像.py

--- 数组/48.旋转图像.py
+++ 数组/48.旋转图像.py
@@ -5,19 +5,10 @@ class Solution:
         """
         n = len(matrix)
         for i in range(n):
-            self.swap_row_col(matrix,i,n)
-        self.swap_row(matrix,n)
-        return matrix
-    
-    def swap_row_col(self, matrix, start,n):
-        for i in range(start,n):
-            matrix[start][i], matrix[i][start] = matrix[i][start], matrix[start][i]
-    
-    def swap_row(self,matrix,n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
         for i in range(n):
             for j in range(n//2):
-                matrix[i][j],matrix[i][n-1-j] =matrix[i][n-1-j] ,matrix[i][j]
+                matrix[i][j],matrix[i][n-j-1] = matrix[i][n-j-1],matrix[i][j]
 
-
-
-# 先交换行和列（第一行第一列，以此类推），再反转每行。
+# 对角线交换，再交换列
