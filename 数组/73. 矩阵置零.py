c487167d0743dc9beb726bb602c2b087bf012f14
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-10 11:21:19 -0700

    Update 73. 矩阵置零.py

--- 数组/73. 矩阵置零.py
+++ 数组/73. 矩阵置零.py
@@ -3,29 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        first_row_has_0 = False
-        first_col_has_0 = False
         m,n = len(matrix),len(matrix[0])
-        for i in range(m):
-            if matrix[i][0]==0:
-                first_col_has_0=True
+        first_row_zero, first_col_zero = False, False
         for j in range(n):
             if matrix[0][j]==0:
-                first_row_has_0=True
+                first_row_zero = True
+                break
+        for i in range(m):
+            if matrix[i][0]==0:
+                first_col_zero = True
+                break
         for i in range(1,m):
             for j in range(1,n):
                 if matrix[i][j]==0:
-                    matrix[0][j]=0
                     matrix[i][0]=0
+                    matrix[0][j]=0
         for i in range(1,m):
             for j in range(1,n):
                 if matrix[0][j]==0 or matrix[i][0]==0:
-                    matrix[i][j]=0
-        if first_row_has_0:
-            for j in range(n):
+                     matrix[i][j]=0
+        for j in range(n):
+            if first_row_zero:
                 matrix[0][j]=0
-        if first_col_has_0:
-            for i in range(m):
+        for i in range(m):
+            if first_col_zero:
                 matrix[i][0]=0
 # 1.先检查第一行和第一列有没有 0，用两个标志变量记录下来。
 # 2.用第一行和第一列当作“标记位”，标记其他行列是否需要置 0。
