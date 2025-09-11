4887ede32d9e276b8d7b189b1790fccae9fed407
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-11 10:25:31 -0700

    Update 66.加一.py

--- 数组/66.加一.py
+++ 数组/66.加一.py
@@ -1,13 +1,18 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in reversed(range(len(digits))):
-            if digits[i]==9:
-                digits[i]=0
-            else:
-                digits[i]+=1
-                return digits
-        digits[0]=1
-        digits.append(0)
+        # [9,9,9]
+        last_digit = digits[-1]
+        sum_ = last_digit+1
+        carry = sum_//10
+        digits[-1] = sum_%10
+        i = len(digits)-2
+        while i>=0 and carry:
+            sum_ = digits[i]+carry
+            carry = sum_//10
+            digits[i] = sum_%10
+            i-=1
+        if carry:
+            return [1]+digits
         return digits
         
 # 199-->200 129-->130 都work
