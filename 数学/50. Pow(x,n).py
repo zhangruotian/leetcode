aafc5714126d0c0eef737aba0d974cd365113c55
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-11 11:04:34 -0700

    Update 50. Pow(x,n).py

--- 数学/50. Pow(x,n).py
+++ 数学/50. Pow(x,n).py
@@ -1,25 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return self.pow(x,n) if n>=0 else 1.0/self.pow(x,-n)
-
-    def pow(self,x,n):
+        if x==0:
+            return 0
         if n==0:
             return 1
-        y=self.pow(x,n//2)
-        return y*y if n%2==0 else y*y*x  
-        # y**2报错：numerical values overflow
-# 递归 T:O(logn) S:O(logn)
+        if n>0:
+            return self.myPowPosN(x,n)
+        return 1/self.myPowPosN(x,-n)
 
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        abs_n=abs(n)
-        res=1
-        while abs_n>0:
-            if abs_n&1:
-                res*=x
-            x=x*x
-            abs_n>>=1
-        return res if n>=0 else 1/res
-        
-# 迭代 T:O(logn) S:O(1)
-#快速幂 笔记
+    def myPowPosN(self,x,n):
+        if n==1:
+            return x
+        half_n = self.myPow(x,n//2)
+        if n%2==0:
+            return half_n*half_n
+        else:
+            return half_n*half_n*x
