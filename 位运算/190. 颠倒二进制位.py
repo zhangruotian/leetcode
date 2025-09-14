8f05de0b2673dc84fb6131f6d0dbca07fa2e0a41
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-14 13:15:42 -0700

    Update 190. 颠倒二进制位.py

--- 位运算/190. 颠倒二进制位.py
+++ 位运算/190. 颠倒二进制位.py
@@ -1,8 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # 101101
         res = 0
         for _ in range(32):
-            res = (res<<1) | (n&1) 
-            n = n>>1
+            res<<=1
+            res+=n&1
+            n>>=1
         return res
+        
