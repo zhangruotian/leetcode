03687524d56e6967113ddb5d1762e991a201d70b
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-14 12:42:12 -0700

    Update 136. 只出现一次的数字.py

--- 位运算/136. 只出现一次的数字.py
+++ 位运算/136. 只出现一次的数字.py
@@ -4,4 +4,8 @@ class Solution:
         for i in range(1,len(nums)):
             res=res^nums[i]
         return res
- #异或
+#1. 任何数和 0 做异或运算，结果仍然是原来的数，即 a⊕0=a。
+#2. 任何数和其自身做异或运算，结果是 0，即 a⊕a=0。
+#3. 异或运算满足交换律和结合律，即 a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b。
+# 对于一连串的异或来说，可以任意调整他们的位置。
+
