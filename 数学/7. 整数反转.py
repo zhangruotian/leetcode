52663396b8022eecfc6cb45c73edc097c8faddd5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-14 18:50:55 -0700

    Update 7. 整数反转.py

--- 数学/7. 整数反转.py
+++ 数学/7. 整数反转.py
@@ -1,17 +1,22 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        neg=False
+        max_ = 2**31-1
+        min_ = -2**31
+        if x==min_: #保证可以x=-x
+            return 0
+        neg = False
         if x<0:
-            x=-x
-            neg=True
-        res=0
+            x = -x
+            neg = True
+        res = 0
         while x>0:
-            tmp=x%10
+            if res>max_/10: #用max_就行。比如max_=14,min_=15。max_/10=1的时候对max_,min_都可以，max_/10=2的时候都不行。
+                return 0
+            res*=10
+            if res>max_-(x%10): #用max_就行。唯一例外可能就是res+(x%10)=min_，但是-8463847412早就应该返回0了，所以用max_就行。
+                return 0
+            res+=(x%10)
             x=x//10
-            if not neg and (res>214748364 or (res==214748364 and tmp>7)):
-                return 0
-            if neg and (res>214748364 or (res==214748364 and tmp>8)):
-                return 0
-            res=res*10+tmp
-        return res if not neg else -res
-#https://leetcode-cn.com/problems/reverse-integer/solution/tu-jie-7-zheng-shu-fan-zhuan-by-wang_ni_ma/
+        return -res if neg else res
+
+        
