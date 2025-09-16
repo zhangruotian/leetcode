9be6caf6b3545a968f5417a75b5b70cbc2bfb365
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-16 16:22:30 -0700

    Update KNN.py

--- 列表/KNN.py
+++ 列表/KNN.py
@@ -1,41 +1,31 @@
-# X_train (n,d) y_train (n,1) X(m,d)
+# X_train (n,d) y_train (n,1) X_test(m,d) KNN
 import numpy as np
-from collections import Counter
-import matplotlib.pyplot as plt
-
+from matplotlib import pyplot as plt
 class KNN:
     def __init__(self,k):
         self.k = k 
-
-    def fit(self,X_train,y_train):
+        
+    def train(self,X_train,y_train):
         self.X_train = X_train
         self.y_train = y_train
+        self.n,self.d = X_train.shape 
 
-    def euclidean_dist(self,X):
-        return np.sqrt(np.sum((self.X_train[:,np.newaxis,:] - X)**2,axis=2))
 
-    def predict(self,X):
-        distances = self.euclidean_dist(X).T 
-        res = []
-        for d in distances:
-            most_close_k_index = np.argsort(d)[:self.k]
-            most_close_k_class = self.y_train[most_close_k_index].flatten()
-            counter = Counter(most_close_k_class)
-            # implement tie-breaker
-            tied_classes = []
-            max_votes_count = counter.most_common()[0][1]
-            for cat,c in counter.items():
-                if c==max_votes_count:
-                    tied_classes.append(cat)
-            if len(tied_classes)==1:
-                res.append(tied_classes[0])
-            else:
-                for cls in most_close_k_class:
-                    if cls in tied_classes:
-                        res.append(cls)
-                        break
+    def euclidean_dist(self,X_train,X_test):
+        X_test = X_test.reshape((self.m,1,self.d))
+        return np.sum((X_train-X_test)**2,axis=2) 
 
-        return np.array(res).reshape(-1,1)
+    def row_mode(self,row):
+        vals, counts = np.unique(row, return_counts=True)
+        return vals[np.argmax(counts)]     
+
+    def predict(self,X_test):
+        self.m,_ = X_test.shape
+        dists = self.euclidean_dist(self.X_train,X_test)
+        top_k = np.argsort(dists,axis=1)[:self.m,:self.k] #m,k y_train (n,1
+        preds = self.y_train[top_k].reshape((self.m,self.k))#m,k
+        return np.apply_along_axis(self.row_mode, 1, preds)
+
 
 X_0 = np.random.randn(10,2) + np.array([-2,-2])
 y_0 = np.zeros((10,1))
@@ -46,19 +36,14 @@ y_train = np.vstack((y_0,y_1))
 X = np.array([[-2,0],[2,0]])
 
 model = KNN(3)
-model.fit(X_train,y_train)
+model.train(X_train,y_train)
 
 y = model.predict(X)
+y = y.reshape((-1,1))
+print(y.shape)
 X_draw = np.vstack((X_train,X))
 
 y_draw = np.vstack((y_train,y))
 plt.scatter(X_draw[:,0],X_draw[0:,1],c=y_draw.flatten())
 plt.scatter(X[:,0],X[:,1],marker='x')
 plt.show()
-
-
-
-
-
-
-
