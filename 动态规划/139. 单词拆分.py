95b2a4fa804c017056d04981b712c127ba6a72ee
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-17 11:28:56 -0700

    Update 139. 单词拆分.py

--- 动态规划/139. 单词拆分.py
+++ 动态规划/139. 单词拆分.py
@@ -5,9 +5,8 @@ class Solution:
         dp[0] = True
         for i in range(n):
             for word in wordDict:
-                if i-len(word)+1>=0 and dp[i-len(word)+1] and word==s[i-len(word)+1:i+1]:
+                if i-len(word)+1>=0 and dp[i-len(word)+1] and word==s[i-len(word)+1:i+1]: #注意判断i-len(word)+1>=0
                     dp[i+1] = True
-                    break
+                    break   # 找到某个词可以匹配后，不需要再看别的词了
         return dp[n]
 
-# https://www.youtube.com/watch?v=H2EgWq-45CY
