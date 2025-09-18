5ce4e86bce1b84e1c279c10d7aa0782cf21fc9d5
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-17 20:07:30 -0700

    Update conv2d.py

--- 列表/conv2d.py
+++ 列表/conv2d.py
@@ -1,53 +1,39 @@
-import numpy as np
+import numpy as np 
 
-def conv2d(x, w, bias, stride, padding):
-    # x: (N, Cin, H, W)
-    # w: (Cout, Cin, kH, kW)
-    # bias: (Cout,) 或 None
-    # stride: (sH, sW)      padding: (pH, pW)
-    N, Cin, H, W = x.shape
-    Cout, _, kH, kW = w.shape
-    sH, sW = stride
-    pH, pW = padding
+class Conv2d:
+    def __init__(self,cin,cout,kh,kw):
+        self.cin = cin
+        self.cout = cout
+        self.kh = kh
+        self.kw = kw
+        self.weights = np.random.randn(cout,cin,kh,kw) #(cout,cin,kh,kw)
+        self.bias = np.random.randn(cout)
 
-    # zero padding
-    xp = np.pad(x, ((0,0),(0,0),(pH,pH),(pW,pW)))
-    Hp, Wp = xp.shape[2], xp.shape[3]
+    def forward(self,X,padding,stride):
+        X = np.pad(X,((0, 0), (0, 0), (padding, padding), (padding, padding))) # X(B,cin,H,W)
+        B,cin,H,W = X.shape
+        X_out_h = (H-self.kh)//stride+1
+        X_out_w = (W-self.kw)//stride+1
+        X_out = np.zeros((B,self.cout,X_out_h,X_out_w))
+        for b in range(B):
+            x = X[b] #(cin,H,W)
+            for c in range(self.cout):
+                weight = self.weights[c] #(cin,kh,kw)
+                bias = self.bias[c] #(1)
+                for i in range(X_out_h):
+                    h_top = i*stride
+                    h_bot = h_top+self.kh
+                    for j in range(X_out_w):
+                        w_left = j*stride
+                        w_right = w_left+self.kw
+                        X_out[b,c,i,j] = np.sum(x[:,h_top:h_bot,w_left:w_right]*weight)+bias
+        return X_out
 
-    # 输出尺寸
-    Hout = (Hp - kH) // sH + 1
-    Wout = (Wp - kW) // sW + 1
-    y = np.zeros((N, Cout, Hout, Wout), dtype=x.dtype)
-
-    for n in range(N):
-        for co in range(Cout):
-            for i in range(Hout):
-                h0 = i * sH
-                h1 = h0 + kH
-                for j in range(Wout):
-                    w0 = j * sW
-                    w1 = w0 + kW
-            
-                    # 1. 提取感受野 (Extract the receptive field)
-                    image_patch = xp[n, :, h0:h1, w0:w1]
-                    
-                    # 2. 提取当前卷积核 (Extract the current filter)
-                    current_filter = w[co]
-                    
-                    # 3. 逐元素相乘并求和 (Element-wise multiply and sum)
-                    y[n, co, i, j] = np.sum(image_patch * current_filter)
-            
-            # 将偏置项加到整个通道上
-            if bias is not None:
-                y[n, co] += bias[co]
-                
-    return y
-
-x=np.ones((1,2,2,2))
-w = np.ones((2,2,2,2))
-bias = np.zeros((2,1))
-stride = (1,1)
-padding = (0,0)
-
-print(conv2d(x,w,bias,stride,padding))
-
+if __name__ == '__main__':
+    X = np.ones((2,2,4,4))
+    model = Conv2d(2,1,2,2)
+    model.weights = np.ones((1,2,2,2))
+    model.bias = np.ones(1)
+    res = model.forward(X,0,2)
+    expected = np.full((2,1,2,2),9)
+    print(np.all(res==expected))
