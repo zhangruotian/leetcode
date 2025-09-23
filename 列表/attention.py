1cff46f93bea0524c5ba693baf8a5018faafff7e
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-22 17:43:26 -0700

    Update attention.py

--- 列表/attention.py
+++ 列表/attention.py
@@ -1,34 +1,34 @@
-import torch
-import torch.nn.functional as F
+# X(B,L,D)
 import torch.nn as nn
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self,num_heads,D):
+import torch
+import torch.nn.functional as F 
+class SelfAttention(nn.Module):
+    def __init__(self,D,num_heads):
         super().__init__()
-        self.num_heads = num_heads
         self.D = D 
-        self.qkv_w = nn.Linear(D,3*D)
-        self.head_D = self.D//self.num_heads
-        self.scale = self.head_D ** -0.5
-        self.out_proj = nn.Linear(D,D)
+        self.wqkv = nn.Linear(self.D,self.D*3)
+        self.num_heads = num_heads
+        self.head_d = self.D//self.num_heads
+        self.scale = self.head_d**-0.5
+        self.proj = nn.Linear(self.D,self.D)
 
-    def forward(self,x):
-        # x(B,L,D)
-        B,L,_ = x.shape
-        qkv = self.qkv_w(x) # (B,L,3D)
-        qkv = qkv.view(B,L,3,self.num_heads,self.head_D)
-        q,k,v = qkv.unbind(2) # (B,L,num_heads,hD)
-        q,k,v = [m.transpose(1,2) for m in (q,k,v)] # (B,num_heads,L,hD)
-
-        attn = torch.matmul(q,k.transpose(-1,-2))*self.scale #(B,num_heads,L,L)
-        mask = torch.triu(torch.ones((L,L),dtype=torch.bool),1) 
+    def forward(self,X):
+        B,L,_ = X.shape #X(B,L,D)
+        qkv = self.wqkv(X) #X(B,L,3D)
+        qkv = qkv.view(B,L,3,self.num_heads,self.head_d) #qkv(B,L,3,#heads,head_d)
+        q,k,v = qkv.unbind(2) # q,k,v(B,L,#heads,head_d)
+        q,k,v = [t.transpose(1,2) for t in (q,k,v)] #q,k,v(B,#heads,L,head_d)
+        attn = q@k.transpose(-1,-2)*self.scale # attn (B,#heads,L,L)
+        mask = torch.triu(torch.ones((L,L),dtype=torch.bool,device=X.device),1)
         attn = attn.masked_fill(mask,-1e9)
-        score = F.softmax(attn,-1) # #(B,num_heads,L,L)
-        print(score)
-        out = torch.matmul(score,v).transpose(1,2).reshape(B,L,self.D) #(B,num_heads,L,hD)
-        return self.out_proj(out)
+        score = F.softmax(attn,-1) # score (B,#heads,L,L)
+        out = score@v # attn (B,#heads,L,head_d)
+        out = out.transpose(1,2).reshape(B,L,self.D)
+        return self.proj(out)
 
-model = MultiHeadAttention(2,4)
-x = torch.rand(4,2,4)
-print(model(x))
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+model = SelfAttention(1024, 8).to(device)
+X = torch.rand(16, 512, 1024).to(device)
+print(model(X))
 
