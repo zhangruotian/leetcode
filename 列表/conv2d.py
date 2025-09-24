c77f36d133ad6ad89b066f1c67a353e5bbe7259f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-23 17:46:58 -0700

    Update conv2d.py

--- 列表/conv2d.py
+++ 列表/conv2d.py
@@ -1,39 +1,35 @@
-import numpy as np 
+import numpy as np
 
 class Conv2d:
-    def __init__(self,cin,cout,kh,kw):
-        self.cin = cin
-        self.cout = cout
-        self.kh = kh
-        self.kw = kw
-        self.weights = np.random.randn(cout,cin,kh,kw) #(cout,cin,kh,kw)
-        self.bias = np.random.randn(cout)
 
-    def forward(self,X,padding,stride):
-        X = np.pad(X,((0, 0), (0, 0), (padding, padding), (padding, padding))) # X(B,cin,H,W)
-        B,cin,H,W = X.shape
-        X_out_h = (H-self.kh)//stride+1
-        X_out_w = (W-self.kw)//stride+1
-        X_out = np.zeros((B,self.cout,X_out_h,X_out_w))
+    def __init__(self,kh,kw,c_in,c_out,padding,stride_h,stride_w):
+        self.kernel = np.random.randn(c_out,c_in,kh,kw) #(c_out,c_in,kh,kw)
+        self.b = np.random.randn(c_out,1,1)
+        self.padding=padding
+        self.stride_h = stride_h
+        self.stride_w = stride_w 
+
+    def forward(self,X):
+        # X(B,C,H,W)
+        X = np.pad(X,((0,0),(0,0),(self.padding,self.padding),(self.padding,self.padding)))
+        B,C,H,W = X.shape
+        c_out,c_in,kh,kw = self.kernel.shape
+        h_out = (H-kh)//self.stride_h+1
+        w_out = (W-kw)//self.stride_w+1
+        out = np.zeros((B,c_out,h_out,w_out))
+
         for b in range(B):
-            x = X[b] #(cin,H,W)
-            for c in range(self.cout):
-                weight = self.weights[c] #(cin,kh,kw)
-                bias = self.bias[c] #(1)
-                for i in range(X_out_h):
-                    h_top = i*stride
-                    h_bot = h_top+self.kh
-                    for j in range(X_out_w):
-                        w_left = j*stride
-                        w_right = w_left+self.kw
-                        X_out[b,c,i,j] = np.sum(x[:,h_top:h_bot,w_left:w_right]*weight)+bias
-        return X_out
+            x = X[b]#(C,H,W)
+            for c in range(c_out):
+                kernel = self.kernel[c] #(C,kh,kw)
+                for i in range(h_out):
+                    for j in range(w_out):
+                        out[b,c,i,j]=np.sum(x[:,i*self.stride_h:i*self.stride_h+kh,j*self.stride_w:j*self.stride_w+kw]*kernel)
+        out += self.b
+        return out 
 
-if __name__ == '__main__':
+if __name__=='__main__':
+    model = Conv2d(2,2,2,2,0,2,2)
     X = np.ones((2,2,4,4))
-    model = Conv2d(2,1,2,2)
-    model.weights = np.ones((1,2,2,2))
-    model.bias = np.ones(1)
-    res = model.forward(X,0,2)
-    expected = np.full((2,1,2,2),9)
-    print(np.all(res==expected))
+    res = model.forward(X)
+    print(res)
