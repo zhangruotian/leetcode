1e3ffaf368edd4c25dcd78774e4938a2b25cd3fb
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-24 12:49:41 -0700

    Update 45. 跳跃游戏 II.py

--- 贪心算法/45. 跳跃游戏 II.py
+++ 贪心算法/45. 跳跃游戏 II.py
@@ -1,13 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        num_jumps = 0
-        last_reached,reached = -1,0
-        while reached<len(nums)-1:
-            new_reached = reached
-            for i in range(last_reached+1,new_reached+1):
-                new_reached = max(new_reached, nums[i]+i)
-            last_reached = reached
-            reached = new_reached
-            num_jumps+=1
-        return num_jumps
+        can_reach = 0
+        i = 0 
+        res = 0
+        while can_reach<len(nums)-1:
+            new_can_reach=can_reach
+            while i<=can_reach:
+                new_can_reach = max(new_can_reach,i+nums[i])
+                i+=1
+            can_reach = new_can_reach
+            res+=1
+        return res
 # greedy，每一跳记录不同位置能到达最远的位置，知道到达len(nums)-1。O(n)
