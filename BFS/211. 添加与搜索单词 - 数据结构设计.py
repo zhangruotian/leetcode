470134834c8993c0ac163219e37ef26835daaf6e
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-25 12:24:47 -0700

    Update 211. 添加与搜索单词 - 数据结构设计.py

--- BFS/211. 添加与搜索单词 - 数据结构设计.py
+++ BFS/211. 添加与搜索单词 - 数据结构设计.py
@@ -1,33 +1,38 @@
-from collections import deque
 class WordDictionary:
+
     def __init__(self):
         self.d = {}
 
     def addWord(self, word: str) -> None:
         cur = self.d
         for c in word:
-            if not c in cur:
-                cur[c] = {}
+            if c not in cur:
+                cur[c]={}
             cur = cur[c]
-        cur['is_word'] = True
+        cur['is_word']=True
 
     def search(self, word: str) -> bool:
-        q = deque()
-        q.append(self.d)
+        curs = [self.d]
         for c in word:
-            for _ in range(len(q)):
-                popped = q.popleft()
-                if c in popped:
-                    q.append(popped[c])
-                if c =='.':
-                    for v in popped.values():
-                        if type(v)==dict:
-                            q.append(v)
-            if not q:
+            new_curs = []
+            for cur in curs:
+                if c in cur:
+                    new_curs.append(cur[c])
+                elif c=='.':
+                    for key,next in cur.items():
+                        if not key=='is_word':
+                            new_curs.append(next)
+            if not new_curs:
                 return False
-        for r in q:
-            if 'is_word' in r:
+            curs = new_curs
+        for cur in curs:
+            if 'is_word' in cur:
                 return True
         return False
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
         
 # 208题加强版，用所有词创建trie。208题cur为一个node，本题注意搜索的时候cur可能有很多个node，需要把它们用list中，再往下走。
