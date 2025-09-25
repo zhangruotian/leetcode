9c0354182661b25edbc56965be8216012ed3ec62
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-25 13:52:07 -0700

    Update 212. 单词搜索 II.py

--- DFS/212. 单词搜索 II.py
+++ DFS/212. 单词搜索 II.py
@@ -1,40 +1,41 @@
 class Solution:
-    def build_trie(self,d,word):
-        cur = d
-        for c in word:
-            if not c in cur:
-                cur[c] = {}
-            cur = cur[c]
-        cur['is_word'] = word
-    
-    def dfs(self,board,i,j,cur,res,m,n,visited):
-        if 'is_word' in cur:
-            res.add(cur['is_word'])
-        if i<0 or i>=m or j<0 or j>=n:
-            return
-        if visited[i][j]:
-            return 
-        if board[i][j] not in cur:
-            return
-        visited[i][j] = 1
-        self.dfs(board,i+1,j,cur[board[i][j]],res,m,n,visited)
-        self.dfs(board,i-1,j,cur[board[i][j]],res,m,n,visited)
-        self.dfs(board,i,j+1,cur[board[i][j]],res,m,n,visited)
-        self.dfs(board,i,j-1,cur[board[i][j]],res,m,n,visited)
-        visited[i][j] = 0
-
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        d = {}
-        res = set()
+        trie = self.build_trie(words)
         m,n = len(board),len(board[0])
-        visited = [[0]*n for _ in range(m)]
-        for word in words:
-            self.build_trie(d,word)
+        res = set()
+        path = []
+        visited = [[False]*n for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if board[i][j] in d:
-                    self.dfs(board,i,j,d,res,m,n,visited)
+                if board[i][j] in trie:
+                    self.dfs(board,i,j,m,n,trie[board[i][j]],res,path,visited)
         return list(res)
+    
+    def dfs(self,board,i,j,m,n,cur,res,path,visited):
+        if visited[i][j]:
+            return
+        visited[i][j]=True
+        path.append(board[i][j])
+        directions = [(0,-1),(0,1),(-1,0),(1,0)]
+        for gi,gj in directions:
+            if 0<=i+gi<m and 0<=j+gj<n and board[i+gi][j+gj] in cur:
+                self.dfs(board,i+gi,j+gj,m,n,cur[board[i+gi][j+gj]],res,path,visited)
+        if 'is_word' in cur:
+            res.add(''.join(path))
+        visited[i][j]=False
+        path.pop()
+    
+    def build_trie(self,words):
+        d = {}
+        for word in words:
+            cur = d
+            for c in word:
+                if c not in cur:
+                    cur[c]={}
+                cur = cur[c]
+            cur['is_word']=True
+        return d
+
 
 
 # 用 Trie 把所有单词的前缀挂起来，再从每个格子做 DFS，只往 Trie 里存在的分支走；遇到完整单词就加入结果并标记删除
