fcce842cf91955efe842103c6b7e774b772a061d
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-29 14:19:59 -0700

    Update 76. 最小覆盖子串.py

--- 滑动窗口/76. 最小覆盖子串.py
+++ 滑动窗口/76. 最小覆盖子串.py
@@ -1,25 +1,23 @@
+from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        from collections import Counter
-        c = Counter(t)
+        count = Counter(t)
+        res_i,res_j = float('-inf'),float('inf')
+        i = 0 
         formed = 0
-        i = 0
-        res_i,res_j = 0,0
-        res_len = float('inf')
         for j in range(len(s)):
-            if s[j] in c: 
-                if c[s[j]]>0:
+            if s[j] in count:
+                count[s[j]]-=1
+                if count[s[j]]==0:
                     formed+=1
-                c[s[j]]-=1
-            while formed == len(t):
-                if j-i+1<res_len:
-                    res_len = j-i+1
-                    res_i,res_j = i,j
-                if s[i] in c:
-                    if c[s[i]]>-1:
-                        formed-=1
-                    c[s[i]] += 1
-                i+=1
-        return "" if res_len == float('inf') else s[res_i:res_j+1]
-
+                while formed==len(count):
+                    if j-i<res_j-res_i:
+                        res_i,res_j = i,j
+                    if s[i] in count:
+                        count[s[i]]+=1
+                        if count[s[i]]==1:
+                            formed-=1
+                    i+=1
+        return s[res_i:res_j+1] if res_i!=float('-inf') else ''
+                
             #https://www.youtube.com/watch?v=YP3bBDuojqk
