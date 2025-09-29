69d911fe754e0ca68430a91bf89510d963d88168
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-29 15:54:31 -0700

    Update 424. 替换后的最长重复字符.py

--- 滑动窗口/424. 替换后的最长重复字符.py
+++ 滑动窗口/424. 替换后的最长重复字符.py
@@ -1,17 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # AACBD BA k=2
-        count = {}
+        res = 1
+        count = {s[0]:1}
         i = 0
-        res = 0
-        for j in range(len(s)):
-            count[s[j]] = count.get(s[j],0)+1
-            if max(count.values())+k >= j-i+1:
-                res = max(res,j-i+1)
+        for j in range(1,len(s)):
+            if s[j] in count:
+                count[s[j]]+=1
             else:
-                while i<j and max(count.values())+k < j-i+1:
-                    count[s[i]] -= 1
-                    i+=1
+                count[s[j]] = 1
+            if max(count.values())+k>=j-i+1:
+                res = max(res,j-i+1)
+            while max(count.values())+k<j-i+1:
+                count[s[i]]-=1
+                i+=1
         return res
 
 # https://www.youtube.com/watch?v=gqXU1UyA8pk
