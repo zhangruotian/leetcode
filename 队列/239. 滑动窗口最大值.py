92eef2010e6c3c7db5d56f81c656a2dab575a88b
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-09-30 10:28:14 -0700

    Update 239. 滑动窗口最大值.py

--- 队列/239. 滑动窗口最大值.py
+++ 队列/239. 滑动窗口最大值.py
@@ -14,23 +14,4 @@ class Solution:
             if i+1>=k:
                 res.append(q[0][0])
         return res
-
-
-# heap O(nlogk)
-import heapq
-class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        max_heap = []
-        res = []
-        for i in range(k):
-            max_heap.append((-nums[i],i))
-
-        heapq.heapify(max_heap)
-        res.append(-max_heap[0][0])
-        for i in range(k,len(nums)):
-            heapq.heappush(max_heap,(-nums[i],i))
-            while max_heap[0][1]<=i-k:
-                heapq.heappop(max_heap)
-            res.append(-max_heap[0][0])
-        return res
         
