7b4ad01693ac4cf29a7b97b1ab77efb3eae78bc9
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-01 12:44:54 -0700

    Update 84. 柱状图中最大的矩形.py

--- 栈/单调栈/84. 柱状图中最大的矩形.py
+++ 栈/单调栈/84. 柱状图中最大的矩形.py
@@ -1,32 +1,26 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
+        right, left = [n]*n,[-1]*n
+        right_stack = [(0,heights[0])]
+        left_stack = [(n-1,heights[-1])]
         res = 0
-        left_stack = [0]
-        extend_left = [i for i in range(n)]
         for i in range(1,n):
-            while left_stack and heights[left_stack[-1]]>=heights[i]:
-                left_stack.pop()
-            if left_stack:
-                extend_left[i] = left_stack[-1]+1
-            else:
-                extend_left[i] = 0
-            left_stack.append(i) 
-
-        right_stack = [n-1]
-        extend_right = [i for i in range(n)]
+            while right_stack and heights[i]<right_stack[-1][1]:
+                pop_i,_ = right_stack.pop()
+                right[pop_i] = i
+            right_stack.append((i,heights[i]))
+        
         for i in range(n-2,-1,-1):
-            while right_stack and heights[right_stack[-1]]>=heights[i]:
-                right_stack.pop()
-            if right_stack:
-                extend_right[i] = right_stack[-1]-1
-            else:
-                extend_right[i] = n-1
-            right_stack.append(i) 
+            while left_stack and heights[i]<left_stack[-1][1]:
+                pop_i,_ = left_stack.pop()
+                left[pop_i] = i
+            left_stack.append((i,heights[i]))
         
         for i in range(n):
-            res = max(res,heights[i]*(extend_right[i]-extend_left[i]+1))
-        return res 
+            res = max(res,(right[i]-left[i]-1)*heights[i])
+        return res
+        
 # 单调栈
 # 核心思想: 对于直方图中的每一个柱子 i，你想找到以它自身高度 heights[i] 为准，能向左和向右延伸的最大宽度。宽度就是 右边界 - 左边界 + 1。
 
