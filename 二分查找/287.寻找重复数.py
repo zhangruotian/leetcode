2d090e9a5318aa35d4f91d3318e59fa2d0ee44a1
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-02 15:34:34 -0700

    Update 287.寻找重复数.py

--- 二分查找/287.寻找重复数.py
+++ 二分查找/287.寻找重复数.py
@@ -1,37 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        l=1
-        r=len(nums)+1
-        while l<r:
-            m=(l+r-1)//2
-            count=0
-            for num in nums:
-                if num<=m:
-                    count+=1
-            if count>m:
-                r=m
-            else:
-                l=m+1
-        return l
-        
-# T:O(nlogn)   S:O(1)
-
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
         slow,fast = nums[0],nums[nums[0]]
-        while True:
-            if slow==fast:
-                break
+        while slow!=fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
-        cur2 = nums[slow]
-        cur1 = nums[0]
-        while True:
-            if cur1==cur2:
-                return cur1
+        cur1,cur2 = 0,slow 
+        while cur1!=cur2:
             cur1 = nums[cur1]
             cur2 = nums[cur2]
+        return cur1
 # T:O(n)   S:O(1)
 # 求链表入环的第一个值
         
