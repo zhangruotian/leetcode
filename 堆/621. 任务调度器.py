c96b37c9082ea00b44eba7585e377afe35443a0f
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-07 15:08:10 -0700

    Update 621. 任务调度器.py

--- 堆/621. 任务调度器.py
+++ 堆/621. 任务调度器.py
@@ -1,19 +1,20 @@
+from collections import Counter,deque
 import heapq
-from collections import deque,Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter = Counter(tasks)
-        max_heap = [-1*c for c in counter.values()]
+        max_heap = [-cnt for cnt in Counter(tasks).values()]
         heapq.heapify(max_heap)
         q = deque()
         time = 0
         while max_heap or q:
-            if max_heap:
-                heap_popped = heapq.heappop(max_heap)
-                if heap_popped+1!=0:
-                    q.append((heap_popped+1,time+n))
             if q and q[0][1]==time:
-                heapq.heappush(max_heap,q.popleft()[0])
+                cnt,_ = q.popleft()
+                heapq.heappush(max_heap,cnt)
+            if max_heap:
+                popped = heapq.heappop(max_heap)
+                popped+=1
+                if popped<0:
+                    q.append((popped,time+n+1))
             time+=1
         return time
 # https://www.youtube.com/watch?v=s8p8ukTyA2I
