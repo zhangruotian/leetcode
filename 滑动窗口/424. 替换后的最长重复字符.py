def2f69ac2b8b0c5ca6b872b6c73eca909520695
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-13 12:11:30 -0700

    Update 424. 替换后的最长重复字符.py

--- 滑动窗口/424. 替换后的最长重复字符.py
+++ 滑动窗口/424. 替换后的最长重复字符.py
@@ -1,18 +1,17 @@
+from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 1
-        count = {s[0]:1}
+        # AABCACCC 1
+        cnt = Counter()
+        res = 0
+        n = len(s)
         i = 0
-        for j in range(1,len(s)):
-            if s[j] in count:
-                count[s[j]]+=1
-            else:
-                count[s[j]] = 1
-            if max(count.values())+k>=j-i+1:
-                res = max(res,j-i+1)
-            while max(count.values())+k<j-i+1:
-                count[s[i]]-=1
+        for j in range(n):
+            cnt[s[j]]+=1
+            while max(cnt.values())+k<j-i+1:
+                cnt[s[i]]-=1
                 i+=1
+            res = max(res,j-i+1)
         return res
 
 # https://www.youtube.com/watch?v=gqXU1UyA8pk
