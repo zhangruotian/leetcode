13b6ab21cfb1865840a033869fbf8a7b772ea30b
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-14 12:41:23 -0700

    Update 435. 无重叠区间.py

--- 数组/435. 无重叠区间.py
+++ 数组/435. 无重叠区间.py
@@ -1,14 +1,16 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x:x[0])
-        last = intervals[0]
+        last = intervals[0][1]
         res = 0
         for i in range(1,len(intervals)):
-            if last[1]>intervals[i][0]:
-                last = [last[0],min(last[1],intervals[i][1])]
+            s,e = intervals[i]
+            if s<last:
                 res+=1
+                if e<last:
+                    last=e
             else:
-                last = intervals[i]
+                last = e
         return res
         
 # 贪心，按前端点排序。往后遍历，如果出现有overlap，保留后端点小的（因为有更小的概率跟再后面的区间发生overlap）。
