d180898c7d8f74159c1caccb8155a8ea3d2813ce
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-13 18:59:46 -0700

    Update 142. 环形链表 II.py

--- 链表/142. 环形链表 II.py
+++ 链表/142. 环形链表 II.py
@@ -5,26 +5,22 @@
 #         self.next = None
 
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        if not head:
-            return 
-        slow=head
-        fast=head
-        s1=head
-        s2=None
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        slow,fast = head,head
+        meet = None
         while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
+            slow = slow.next
+            fast = fast.next.next
             if slow is fast:
-                if slow==head:
-                    return head
-                s2=slow
+                meet = slow
                 break
-        if not s2:
-            return 
-        while True:
-            s1=s1.next
-            s2=s2.next
-            if s1 is s2:
-                return s1
+        if not meet:
+            return None
+        cur1,cur2 = head,meet
+        while cur1!=cur2:
+            cur1 = cur1.next
+            cur2 = cur2.next
+        return cur1
 # 快慢指针
