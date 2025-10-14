f22229e4b7e90c75d0503b74a02a47355f0bdd83
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-13 21:58:49 -0700

    Update 78.子集.py

--- 回溯算法/78.子集.py
+++ 回溯算法/78.子集.py
@@ -1,42 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        path=[]
-        res=[]
-        self.backTrack(path,res,0,nums)
+        #.  1.   2.   3 
+        #. 2 3.  3
+        # 3
+        path,res = [],[[]]
+        self.dfs(nums,path,res,0)
         return res
     
-    def backTrack(self,path,res,index,nums):
-        res.append(path[:])
-        if index==len(nums):
-            return
+    def dfs(self,nums,path,res,index):
         for i in range(index,len(nums)):
             path.append(nums[i])
-            self.backTrack(path,res,i+1,nums)
+            res.append(path[:])
+            self.dfs(nums,path,res,i+1)
             path.pop()
-            
-# backtrack
-
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for num in nums:
-            for i in range(len(res)):
-                res.append(res[i]+[num])
-        return res
-# 直接模拟
-
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return [[]]
-        if len(nums)==1:
-            return [nums,[]] 
-        new_list=[]
-        num=[nums[0]]
-        original_list=self.subsets(nums[1:])
-        for i in original_list:
-            new_list.append(num+i)
-        new_list+=original_list
-
-        return new_list
-#divide and conquer
