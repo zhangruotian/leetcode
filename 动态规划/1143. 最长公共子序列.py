4bcbbc95aba7d7957086e838842935af1787774a
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-14 11:23:34 -0700

    Update 1143. 最长公共子序列.py

--- 动态规划/1143. 最长公共子序列.py
+++ 动态规划/1143. 最长公共子序列.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # if text1[i]==text2[j]:
-        #   dp[i][j]=dp[i-1][j-1]+1
-        # else:
-        #   dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        n1,n2=len(text1),len(text2)
-        dp=[[0]*(n1+1) for _ in range(n2+1)]
-        for i in range(n1):
-            for j in range(n2):
-                if text1[i]==text2[j]:
-                    dp[j+1][i+1]=dp[j][i]+1
+        # abcde
+        # bce
+        # dp[i][j] = dp[i-1][j-1]+1
+        # dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        m,n = len(text1), len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] +1 
                 else:
-                    dp[j+1][i+1]=max(dp[j][i+1],dp[j+1][i])
-        return dp[n2][n1]
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[-1][-1]
