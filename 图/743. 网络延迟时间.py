d3794e90b72d7b164cf3f8d9184f6652a7825545
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-14 18:25:07 -0700

    Update 743. 网络延迟时间.py

--- 图/743. 网络延迟时间.py
+++ 图/743. 网络延迟时间.py
@@ -2,29 +2,27 @@ import heapq
 from collections import defaultdict
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-        for s,e,t in times:
-            graph[s].append((e,t))
-
-        res = [float('inf')]*(n+1)
-        res[k] = 0
-        min_heap = [(0,k)]
+        dists = [float('inf')]*(n+1)
+        dists[0] = 0
+        dists[k] = 0
         visited = [0]*(n+1)
-        while sum(visited)<n:
-            if not min_heap:
-                return -1
-            cdist,node = heapq.heappop(min_heap)
-            if cdist<res[node]:
+        min_heap = []
+        graph = defaultdict(list)
+        for s,e,d in times:
+            graph[s].append((d,e))
+            if s==k:
+                dists[e] = d
+                heapq.heappush(min_heap,(d,e))
+        while min_heap:
+            d,e = heapq.heappop(min_heap)
+            if visited[e]:
                 continue
-            visited[node]=1
-            for child,edge in graph[node]:
-                if visited[child]:
-                    continue
-                if cdist+edge<res[child]:
-                    res[child] = cdist+edge
-                    heapq.heappush(min_heap,(cdist+edge,child))
-        longest = max(res[1:])
-        return -1 if longest==float('inf') else longest
+            visited[e]=1
+            for child_d,child_e in graph[e]:
+                if d+child_d<dists[child_e] and not visited[child_e]:
+                    dists[child_e] = d+child_d
+                    heapq.heappush(min_heap,(dists[child_e],child_e))
+        return -1 if max(dists)==float('inf') else max(dists)
       
 # single source shortest path to every vertex
 # all weights are positive
