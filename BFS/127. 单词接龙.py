99890031aa17cd2b237b756582c828b0c5c061af
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-21 14:56:57 -0700

    Update 127. 单词接龙.py

--- BFS/127. 单词接龙.py
+++ BFS/127. 单词接龙.py
@@ -1,33 +1,33 @@
 from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        res = 0
-        q = deque([beginWord])
-        n = len(wordList)
         wordSet = set(wordList)
-        alphas = list(map(chr,range(ord('a'),ord('z')+1)))
-        for i in range(n+1):
-            if not q:
-                return 0
+        visited = {word:False for word in wordList}
+        alpha= list(map(chr,range(ord('a'),ord('z')+1)))
+        if endWord not in wordSet:
+            return 0
+        cnt = 1
+        q = deque()
+        q.append(beginWord)
+        visited[beginWord] = True
+        while q:
             for _ in range(len(q)):
                 popped = q.popleft()
-                if popped == endWord:
-                    return i+1
-                for next_word in self.find_next_words(popped,wordSet,alphas):
+                for next_word in self.get_next_word(popped,visited,wordSet,alpha):
+                    if next_word==endWord:
+                        return cnt+1
                     q.append(next_word)
-                    wordSet.remove(next_word)
+                    visited[next_word]=True
+            cnt+=1
         return 0
-    
-    def find_next_words(self,popped,wordSet,alphas):
+
+    def get_next_word(self,popped,visited,wordSet,alpha):
         next_words = []
         for i in range(len(popped)):
-            for c in alphas:
-                if c==popped[i]:
-                    continue
-                maybe_next = popped[:i]+c+popped[i+1:]
-                if maybe_next in wordSet:
-                    next_words.append(maybe_next)
+            for al in alpha:
+                new_word = popped[:i] + al + popped[i+1:]
+                if new_word in wordSet and not visited[new_word]:
+                    next_words.append(new_word)
         return next_words
 # DFS适用于沿着一条路track完整的路径path，并且用visited保证这条路径不重复添加word。DFS一般要走完整个图/树才能知道结果。本题让找最短的，因此适合BFS,因为在某层找到符合要求的endWord就可以停止了。
-# BFS过程中注意中，不需要知道某个word是上层谁的child。可以去重保障q上无duplicates，而且无上层已访问过的word，因为最终的结果肯定是没有重复word的。
-# https://www.youtube.com/watch?v=hB_nYXFtwP0
+
