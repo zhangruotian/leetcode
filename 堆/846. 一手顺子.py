2c252f8d3f9427cf18e490fc3edb701967470f77
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-22 14:03:53 -0700

    Update 846. 一手顺子.py

--- 堆/846. 一手顺子.py
+++ 堆/846. 一手顺子.py
@@ -2,23 +2,20 @@ from collections import Counter
 import heapq
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand)%groupSize!=0:
+        n = len(hand)
+        if n%groupSize!=0:
             return False
-        num_group = len(hand)//groupSize
-
-        count = Counter(hand)
-        min_heap = list(count.keys())
+        counter = Counter(hand)
+        min_heap = list(counter.keys())
         heapq.heapify(min_heap)
-
-        for _ in range(num_group):
+        for _ in range(n//groupSize):
+            while counter[min_heap[0]]==0:
+                heapq.heappop(min_heap)
             start = min_heap[0]
-            for _ in range(groupSize):
-                if not start in count or count[start]==0:
+            for i in range(groupSize):
+                if counter[i+start]==0:
                     return False
-                count[start]-=1
-                if count[start]==0 and start==min_heap[0]:
-                    heapq.heappop(min_heap)
-                start+=1
+                counter[start+i]-=1
         return True
 # min heap确定每个group的start value（当前剩余卡的最小值）， dict/counter 查询是否存在构成连续所需要的卡。
 # O(nlogn)
