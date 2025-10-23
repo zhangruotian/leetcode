806755f7ed8f9113df5fbd99a5df2fd38a35e7c9
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-22 17:30:52 -0700

    Update 1899. 合并若干三元组以形成目标三元组.py

--- 贪心算法/1899. 合并若干三元组以形成目标三元组.py
+++ 贪心算法/1899. 合并若干三元组以形成目标三元组.py
@@ -1,13 +1,16 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        t1,t2,t3 = target
-        v1,v2,v3 = 0,0,0
-        for triplet in triplets:
-            tp1,tp2,tp3 = triplet
-            if tp1>t1 or tp2>t2 or tp3>t3:
-                continue
-            v1,v2,v3 = max(v1,tp1),max(v2,tp2),max(v3,tp3)
-        return [v1,v2,v3]==target
+        first, second, third = False, False, False
+        ta,tb,tc = target
+        for a,b,c in triplets:
+            if a==ta and b<=tb and c<=tc:
+                first=True
+            if b==tb and a<=ta and c<=tc:
+                second=True
+            if c==tc and a<=ta and b<=tb:
+                third=True
+            if first and second and third:
+                return True
+        return False
 
-# triplet的三个元素中有任何一个比target中对应的元素大，这个元素一定不是有用的，跳过
-# 剩余的triplets中，看所有triplets的第一个元素的最大值是不是target，看第二个最大，第三个最大，如果都是返回true
+# 对于a来说，要有a==ta，并且b,c都要<=tb,tc，这时候证明找到了可用的a。对于b，c来说也是同样的过程。
