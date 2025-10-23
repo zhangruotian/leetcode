c11c2937791a3261be63fd73d461383dbec0cc13
zhangruotian <49586834+zhangruotian@users.noreply.github.com>
2025-10-22 20:42:48 -0700

    Update 678. 有效的括号字符串.py

--- 贪心算法/678. 有效的括号字符串.py
+++ 贪心算法/678. 有效的括号字符串.py
@@ -20,3 +20,28 @@ class Solution:
 # max_left：假设'*'是'('，到此为止左侧最多的左括号数量，如果<0，说明右括号太多，没能力match，返回false，比如(*)))
 # min_left: 假设'*'是')'，到此为止左侧最少的左括号数量，如果<0，说明把前面的'*'都当作')'有点过了。把min_left=0，因为可以把前面的'*'改成空格或者'('，说明到目前为止可以
 # perfect match，往后接着看。如果后面(或者从一开始到现在)，min_left一直>0，说明左括号太多了，匹配不完，返回false，比如(((*)。
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        num_left,num_star = 0,0
+        for c in s:
+            if c=='(':
+                num_left+=1
+            elif c==')':
+                num_left-=1
+            else:
+                num_star+=1
+            if num_left+num_star<0:
+                return False
+        num_right,num_star = 0,0
+        for c in s[::-1]:
+            if c=='(':
+                num_right-=1
+            elif c==')':
+                num_right+=1
+            else:
+                num_star+=1
+            if num_right+num_star<0:
+                return False
+        return True
+# 双向扫描，从左往右检查先出现')'导致失败的情况，从右往左检查先出现'('导致失败的情况。
